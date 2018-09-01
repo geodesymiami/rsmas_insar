0c789e2546f2627f5812960cb9984f65bde222c0
falkamelung <famelung@rsmas.miami.edu>
2018-09-01 11:27:44 -0400

    creates a symbolic link for slcDir if given and different to standar location

---
+++ __init__.py
@@ -0,0 +1,19 @@
+# LOGGING
+import logging
+logging.basicConfig(filename="example.log",
+                            format='%(asctime)s | %(name)-25s | [ %(levelname)s ]'
+                                                       ' | %(filename)s:%(lineno)d | %(message)s',
+                                                                           level=logging.DEBUG)
+
+ch = logging.StreamHandler()
+verbose = False
+if verbose:
+    ch.setLevel(logging.DEBUG)
+else:
+    ch.setLevel(logging.ERROR)
+warning_logger = logging.getLogger("process_sentinel")
+warning_logger.addHandler(ch)
+logger = logging.getLogger("process_sentinel." + "__init__")
+
+logger.debug('Starting Logger')
+logger.error('YO WHATS GOOD?')
