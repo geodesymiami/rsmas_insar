9b4a9e8bb8f84762431700eb4fb05ea83ec800cd
kawanamelung <kawan.amelung@gmail.com>
2018-09-17 13:01:12 -0400

    update

--- dem_rsmas_kawan.py
+++ dem_rsmas_kawan.py
@@ -1,6 +1,6 @@
-#! /usr/bin/env python2
+#! /usr/bin/env python3
 ###############################################################################
-# 
+#
 # Project: dem_ssara_kawan.py
 # Author: Kawan & Falk Amelung
 # Created:9/2018
@@ -11,18 +11,15 @@
 import os
 import sys
 import glob
-import time
 import argparse
-import warnings
 import shutil
 import subprocess
 import messageRsmas
+import re
 from pysar.utils import readfile
 
 
-import re
-
-EXAMPLE='''
+EXAMPLE = '''
   example:
   dem_rsmas.py  $SAMPLES/GalapagosT128SenVVD.template
 
@@ -39,7 +36,7 @@ EXAMPLE='''
 
 '''
 
-xmltext='''
+xmltext = '''
 <imageFile>
     <property name="ISCE_VERSION">
         <value>Release: 2.0.0_20170403, svn-2256, 20170403. Current: svn-exported.</value>
@@ -164,7 +161,7 @@ xmltext='''
     </property>
 </imageFile>'''
 
-vrttext='''
+vrttext = '''
 <VRTDataset rasterXSize="{c1size}" rasterYSize="{c2size}">
     <SRS>{srs}</SRS>
     <GeoTransform>{geotransform}</GeoTransform>
@@ -178,10 +175,12 @@ vrttext='''
 </VRTDataset>'''
 
 ##########################################################################
+
+
 def dem_parser():
     parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter,
-                                    description='Options for SSARA, ISCE, and more? Default set to ISCE.',
-                                    epilog=EXAMPLE)
+                                     description='Options for SSARA, ISCE, and more? Default set to ISCE.',
+                                     epilog=EXAMPLE)
     parser.add_argument('custom_template_file',
                         nargs='?',
                         help='custom template with option settings.\n')
@@ -193,9 +192,9 @@ def dem_parser():
                         dest='isce',
                         action='store_true',
                         help='run isce, set as default')
-    
+
     inps = parser.parse_args()
-    
+
     # set default to ISCE
     if inps.ssara:
         inps.isce = False
@@ -206,43 +205,43 @@ def dem_parser():
 
 def make_dem_dir():
     if os.path.isdir('DEM'):
-       shutil.rmtree('DEM')
+        shutil.rmtree('DEM')
     os.mkdir('DEM')
     os.chdir('DEM')
     return os.getcwd()
 
 
-def grd_to_xml_vrt(cwd):
+def grd_to_xml(cwd):
     print('you have started grd_to_xml')
 
     filename = 'dem.grd'
     tempfile = 'dem.temp'
     outfilexml = 'dem.dem.wgs84.xml'
     outfilevrt = 'dem.dem.wgs84.vrt'
-    command = '/nethome/famelung/test/test/rsmas_insar/3rdparty/python/anaconda2/bin/gdalinfo {file} >> {tempfile}'.format(file= filename,tempfile=tempfile)
+    command = '/nethome/famelung/test/test/rsmas_insar/3rdparty/python/anaconda2/bin/gdalinfo {file} >> {tempfile}'.format(file=filename, tempfile=tempfile)
     subprocess.Popen(command, shell=True).wait()
 
-    with(open(tempfile,'r')) as temp:
-        fulldict= dict()
-        tempstr= temp.read()
+    with(open(tempfile, 'r')) as temp:
+        fulldict = dict()
+        tempstr = temp.read()
 
-        fulldict['c1delta']= re.findall(r'Pixel Size = \((.+),.+\)', tempstr)[0] 
-        fulldict['c1ev']= round(float(re.findall(r'Upper Right\s+\( (.\d+.\d+),', tempstr)[0]),1)
-        fulldict['c1size']= int(re.findall(r'Size is (\d+),\s+\d+',tempstr)[0])  
-        fulldict['c1sv']= round(float(re.findall(r'Lower Left\s+\( (.\d+.\d+),', tempstr)[0]),1)
+        fulldict['c1delta'] = re.findall(r'Pixel Size = \((.+),.+\)', tempstr)[0] 
+        fulldict['c1ev'] = round(float(re.findall(r'Upper Right\s+\( (.\d+.\d+),', tempstr)[0]), 1)
+        fulldict['c1size'] = int(re.findall(r'Size is (\d+),\s+\d+', tempstr)[0])  
+        fulldict['c1sv'] = round(float(re.findall(r'Lower Left\s+\( (.\d+.\d+),', tempstr)[0]), 1)
 
-        fulldict['c2delta']= re.findall(r'Pixel Size = \(.+,(.+)\)', tempstr)[0]
-        fulldict['c2ev']= round(float(re.findall(r'Lower Left\s+\( .\d+.\d+,\s+(.\d+.\d+)\)', tempstr)[0]),1)
-        fulldict['c2size']= int(re.findall(r'Size is \d+,\s+(\d+)',tempstr)[0])
-        fulldict['c2sv']= round(float(re.findall(r'Upper Right\s+\( .\d+.\d+,\s+(.\d+.\d+)\)', tempstr)[0]),1)
+        fulldict['c2delta'] = re.findall(r'Pixel Size = \(.+,(.+)\)', tempstr)[0]
+        fulldict['c2ev'] = round(float(re.findall(r'Lower Left\s+\( .\d+.\d+,\s+(.\d+.\d+)\)', tempstr)[0]), 1)
+        fulldict['c2size'] = int(re.findall(r'Size is \d+,\s+(\d+)', tempstr)[0])
+        fulldict['c2sv'] = round(float(re.findall(r'Upper Right\s+\( .\d+.\d+,\s+(.\d+.\d+)\)', tempstr)[0]), 1)
 
-        fulldict['numbands']= re.findall(r'Band (\d+) \w', tempstr)[0]
-        fulldict['ref']= re.findall(r'GEOGCS\["(.+)",', tempstr)[0].replace(' ','')
-        fulldict['length']= fulldict['c2size']
-        fulldict['width']= fulldict['c1size']
-        fulldict['xmax']= fulldict['c1ev']
-        fulldict['xmin']= fulldict['c1sv']
-        fulldict['filename'] = cwd + '/' +  outfilexml
+        fulldict['numbands'] = re.findall(r'Band (\d+) \w', tempstr)[0]
+        fulldict['ref'] = re.findall(r'GEOGCS\["(.+)",', tempstr)[0].replace(' ', '')
+        fulldict['length'] = fulldict['c2size']
+        fulldict['width'] = fulldict['c1size']
+        fulldict['xmax'] = fulldict['c1ev']
+        fulldict['xmin'] = fulldict['c1sv']
+        fulldict['filename'] = cwd + '/' + outfilexml
         fulldict['extrafilename'] = cwd + '/' + outfilevrt
 
     #    fulldict['srs']= ':'.join (thing for thing in re.findall(r'UNIT.+\s.+\s.+AUTHORITY\["(\w+)","(\d+)"]]',tempstr)[0])
@@ -253,15 +252,15 @@ def grd_to_xml_vrt(cwd):
 
     os.remove(tempfile)
 
-   # with(open(outfilevrt, 'w')) as out:
-    #    out.write(vrttext.format(**fulldict))
+    # with(open(outfilevrt, 'w')) as out:
+    #     out.write(vrttext.format(**fulldict))
 
-    with(open(outfilexml,'w')) as out:
+    with(open(outfilexml, 'w')) as out:
         out.write(xmltext.format(**fulldict))
     return
 
 
-def grd_to_i2():
+def grd_to_envi_vrt():
     command = 'gdal_translate -ot Int16 -of ENVI dem.grd dem.dem.wgs84;gdal_translate -of vrt dem.grd dem.dem.wgs84.xml.vrt' 
     print('command currently executing: ' + command)
     subprocess.Popen(command, shell=True).wait()
@@ -270,15 +269,13 @@ def grd_to_i2():
 
 def call_ssara_dem(custom_template, inps, cwd):
     print('You have started SSARA!')
-    
-    parent_dir = os.getenv('PARENTDIR')    
     out_file = 'ssara_dem.log'
     command = 'ssara_federated_query.py {ssaraopt} --dem >& {outfile}'.format(ssaraopt=custom_template['ssaraopt'], outfile=out_file)
     print('command currently executing: ' + command)
-    status = subprocess.Popen(command, shell=True).wait()
+    subprocess.Popen(command, shell=True).wait()
     print('dem.grd downloaded')
-    grd_to_i2()
-    grd_to_xml_vrt(cwd)
+    grd_to_envi_vrt()
+    grd_to_xml(cwd)
     return
 
 
@@ -288,60 +285,58 @@ def main(argv):
     inps = dem_parser()
     custom_template = readfile.read_template(inps.custom_template_file)
     cwd = make_dem_dir()
-  #  import pdb; pdb.set_trace()
-   # can sentinelStack.demMethod be removed? I think parser is the replacement
+    # can sentinelStack.demMethod be removed? I think parser is the replacement
     if 'sentinelStack.demMethod' not in custom_template.keys():
-        custom_template['sentinelStack.demMethod']='?'
+        custom_template['sentinelStack.demMethod'] = '?'
 
-    if custom_template['sentinelStack.demMethod']=='bbox' or custom_template['sentinelStack.demMethod']=='isce' or inps.isce:
+    if custom_template['sentinelStack.demMethod'] == 'bbox' or custom_template['sentinelStack.demMethod'] == 'isce' or inps.isce:
         print('You hace started isce')
-        bbox=custom_template['sentinelStack.boundingBox']
-        south=bbox.split(' ')[0].split('\'')[1]   # assumes quotes '-1 0.15 -91.3 -91.0'
-        north=bbox.split(' ')[1]
-        west =bbox.split(' ')[2]
-        east =bbox.split(' ')[3].split('\'')[0]
-    
-        south=round(float(south)-0.5)
-        north=round(float(north)+0.5)
-        west =round(float(west)-0.5)
-        east =round(float(east)+0.5)
+        bbox = custom_template['sentinelStack.boundingBox']
+        south = bbox.split(' ')[0].split('\'')[1]   # assumes quotes '-1 0.15 -91.3 -91.0'
+        north = bbox.split(' ')[1]
+        west = bbox.split(' ')[2]
+        east = bbox.split(' ')[3].split('\'')[0]
 
-        demBbox=str(int(south))+' '+str(int(north))+' '+str(int(west))+' '+str(int(east))
-        cmd ='dem.py -a stitch -b '+demBbox+' -c -u https://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL1.003/2000.02.11/'
+        south = round(float(south) - 0.5)
+        north = round(float(north) + 0.5)
+        west = round(float(west) - 0.5)
+        east = round(float(east) + 0.5)
+
+        demBbox = str(int(south)) + ' ' + str(int(north)) + ' ' + str(int(west)) + ' ' + str(int(east))
+        cmd = 'dem.py -a stitch -b ' + demBbox + ' -c -u https://e4ftl01.cr.usgs.gov/MEASURES/SRTMGL1.003/2000.02.11/'
         messageRsmas.log(cmd)
 
         try:
             output = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True, universal_newlines=True)
         except subprocess.CalledProcessError as exc:
-            print("Command failed. Exit code, StdErr:", exc.returncode,exc.output)
+            print("Command failed. Exit code, StdErr:", exc.returncode, exc.output)
             sys.exit('Error produced by dem.py')
         else:
-           #print("Success.        StdOut \n{}\n".format(output))
+            # print("Success.        StdOut \n{}\n".format(output))
             if 'Could not create a stitched DEM. Some tiles are missing' in output:
                 os.chdir('..')
                 shutil.rmtree('DEM')
                 sys.exit('Error in dem.py: Tiles are missing. Ocean???')
 
         xmlFile = glob.glob('demLat_*.wgs84.xml')[0]
-        fin = open(xmlFile,'r')
+        fin = open(xmlFile, 'r')
         fout = open("tmp.txt", "wt")
         for line in fin:
-            fout.write( line.replace('demLat', cwd+'/demLat') )
+            fout.write(line.replace('demLat', cwd + '/demLat'))
         fin.close()
         fout.close()
-        os.rename('tmp.txt',xmlFile)
+        os.rename('tmp.txt', xmlFile)
 
-    elif custom_template['sentinelStack.demMethod']=='ssara' or inps.ssara:
+    elif custom_template['sentinelStack.demMethod'] == 'ssara' or inps.ssara:
         call_ssara_dem(custom_template, inps, cwd)
         print('You have finished SSARA!')
     else:
-        sys.ext('Error unspported demMethod option: '+custom_template['sentinelStack.demMethod'])
- 
-    print '\n###############################################'
-    print 'End of dem_rsmas.py'
-    print '################################################\n'
+        sys.ext('Error unspported demMethod option: ' + custom_template['sentinelStack.demMethod'])
+
+    print('\n###############################################')
+    print('End of dem_rsmas.py')
+    print('################################################\n')
 
 ###########################################################################################
 if __name__ == '__main__':
     main(sys.argv[:])
-
