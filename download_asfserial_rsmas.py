aecbe55276c3653d5b479dc59b908d238660dee7
Joshua Zahner <joshuazahner@gmail.com>
2018-09-29 23:48:59 -0400

    Added proper docstrings to edownload_asfserial_rsmas.py

--- download_asfserial_rsmas.py
+++ download_asfserial_rsmas.py
@@ -6,9 +6,14 @@ import subprocess
 sys.path.insert(0, os.getenv('SSARAHOME'))
 import password_config as password
 
-
-if __name__ == "__main__":
+def generate_files_csv():
+	""" Generates a csv file of the files to download serially.
 	
+		Uses the `awk` command to generate a csv file containing the data files to be download
+		serially. The output csv file is then sent through the `sed` command to remove the first five
+		empty values to eliminate errors in download_ASF_serial.py.
+	
+	"""
 	with open(sys.argv[1], 'r') as template_file:
 		options = ''
 		for line in template_file:
@@ -20,7 +25,25 @@ if __name__ == "__main__":
 	csv_command = ' '.join(filecsv_options)
 	filescsv_status = subprocess.Popen(csv_command, shell=True).wait()
 	sed_command = "sed 's/^.\{5\}//' files.csv > new_files.csv";
+	
 	subprocess.Popen(sed_command, shell=True).wait()
 	
-	serial_status = subprocess.Popen(['download_ASF_serial.py', '-username', password.asfuser, '-password', password.asfpass, 'new_files.csv']).wait()
+def run_download_asf_serial():
+	""" Runs download_ASF_serial.py with proper files.
+	
+		Runs adapted download_ASF_serial.py with a CLI username and password and a csv file containing
+		the the files needed to be downloaded (provided by ssara_federated_query.py --print)
+		
+	"""
+	
+	status = subprocess.Popen(['download_ASF_serial.py', '-username', password.asfuser, '-password', password.asfpass, 'new_files.csv']).wait()
+	
+	return status
+
+if __name__ == "__main__":
+	
+	generate_files_csv()
+	run_download_asf_serial()
+	
+	
 	
