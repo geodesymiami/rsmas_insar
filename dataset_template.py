2b478904ba6bf8d633053a6fd83a26615b86297f
Joshua Zahner <joshuazahner@gmail.com>
2018-10-03 12:03:43 -0400

    Created Template object
    The template object reads in and parses dataset.template files and stores them as a dictionary keyed on the option name. This should allow for a standardized means of accessing template parameters in an efficient manner.

---
+++ dataset_template.py
@@ -0,0 +1,35 @@
+
+class Template:
+
+    options = {}
+
+    def __init__(self, template_file):
+
+        self.options['dataset'] = template_file.split('/')[-1].split(".")[0]
+
+        with open(template_file) as template:
+
+            for line in template:
+
+                if "####################" not in line:
+
+                    parts = line.split("=")
+
+                    key = parts[0].rstrip()
+                    value = parts[1].rstrip().split("#")[0].strip(" ")
+
+                    self.options[str(key)] = value
+
+    def get_options(self):
+        return self.options
+
+    def get_dataset_name(self):
+        return self.options['dataset']
+
+
+
+if __name__ == "__main__":
+
+    temp = Template("/Users/joshua/Desktop/pysar/template_files/GalapagosSenDT128.template")
+
+    print(temp.get_dataset_name())
