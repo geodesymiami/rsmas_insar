6bac7908d38de79208ece9aba315844e19bb0d50
Joshua Zahner <joshuazahner@gmail.com>
2018-10-21 21:10:02 -0400

    Removed extra dest parameters from argument parser

--- download_ssara_rsmas.py
+++ download_ssara_rsmas.py
@@ -24,7 +24,7 @@ def create_parser():
     """ Creates command line argument parser object. """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('template', dest='template', metavar="FILE", help='template file to use.')
+    parser.add_argument('template', metavar="FILE", help='template file to use.')
 
     return parser
 
