6dee9405fe194163e30b978b2be1eaae302036be
Sara Mirzaee <37273875+mirzaees@users.noreply.github.com>
2018-11-09 10:37:00 -0500

    Create ingest_insarmaps.py

---
+++ ingest_insarmaps.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python3
+########################
+# Author: Sara Mirzaee
+#######################
+
+
+import os
+import subprocess
+import sys
+import glob
+import shutil
+import argparse
+from rsmas_logging import rsmas_logger, loglevel
+
+sys.path.insert(0, os.getenv('SSARAHOME'))
+import password_config as password
+
+from _processSteps import create_or_update_template
+from _process_utilities  import get_work_directory, get_project_name
+
+
+logfile_name = os.getenv('OPERATIONS') +'/LOGS/ingest_insarmaps.log'
+logger = rsmas_logger(file_name=logfile_name)
+
+
+##############################################################################
+EXAMPLE = """example:
+  ingest_insarmaps.py LombokSenAT156VV.template 
+"""
+
+def create_parser():
+    """ Creates command line argument parser object. """
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, epilog=EXAMPLE)
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1')
+    parser.add_argument('custom_template_file', nargs='?',
+                        help='custom template with option settings.\n')
+
+    return parser
+
+
+def command_line_parse(args):
+    """ Parses command line agurments into inps variable. """
+
+    parser = create_parser()
+    inps = parser.parse_args(args)
+    return inps
+
+
+
+if __name__ == "__main__":
+
+    inps = command_line_parse(sys.argv[1:])
+    inps.project_name = get_project_name(inps.custom_template_file)
+    inps.work_dir = get_work_directory(None, inps.project_name)
+    inps = create_or_update_template(inps)
+
+    os.chdir(inps.work_dir)
+
+    hdfeos_file = glob.glob(inps.work_dir + '/PYSAR/S1*.he5')
+    hdfeos_file.append(glob.glob(inps.work_dir +'/PYSAR/SUBSET_*/S1*.he5'))
+    hdfeos_file = hdfeos_file[0]
+
+    json_folder = inps.work_dir + '/PYSAR/JSON'
+    mbtiles_file = json_folder + '/' + os.path.splitext(os.path.basename(hdfeos_file))[0] + '.mbtiles'
+
+    if os.path.isdir(json_folder):
+        logger.info('Removing directory: %s', json_folder)
+        shutil.rmtree(json_folder)
+
+    command1 = 'hdfeos5_2json_mbtiles.py ' + hdfeos_file + ' ' + json_folder + ' |& tee out_insarmaps.log'
+    command2 = 'json_mbtiles2insarmaps.py -u ' + password.insaruser + ' -p ' + password.insarpass + ' --host ' + \
+               'insarmaps.miami.edu -P rsmastest -U rsmas\@gmail.com --json_folder ' + \
+               json_folder + ' --mbtiles_file ' + mbtiles_file + ' |& tee -a out_insarmaps.log'
+
+    with open(inps.work_dir + '/PYSAR/run_insarmaps', 'w') as f:
+        f.write(command1 + '\n')
+        f.write(command2 + '\n')
+
+
+    status = subprocess.Popen(command1, shell=True).wait()
+    if status is not 0:
+        logger.log(loglevel.ERROR, 'ERROR in hdfeos5_2json_mbtiles.py')
+        raise Exception('ERROR in hdfeos5_2json_mbtiles.py')
+
+
+    status = subprocess.Popen(command2, shell=True).wait()
+    if status is not 0:
+        logger.log(loglevel.ERROR, 'ERROR in json_mbtiles2insarmaps.py')
+        raise Exception('ERROR in json_mbtiles2insarmaps.py')
+
+
+    logger.log(loglevel.INFO, "-----------------Done ingesting insarmaps-------------------")
