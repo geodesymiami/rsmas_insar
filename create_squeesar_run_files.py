6505dfa1f4de15f26f3003d120f7f52585dd2f28
Sara Mirzaee <sxm1611@login4.pegasus.edu>
2019-02-06 15:06:30 -0500

    Updated to addsqueesar

--- create_squeesar_run_files.py
+++ create_squeesar_run_files.py
@@ -16,14 +16,11 @@ from _processSteps import create_or_update_template, create_or_copy_dem
 from _process_utilities  import get_work_directory, get_project_name
 from _process_utilities  import _remove_directories, send_logger
 
-sys.path.insert(0, os.getenv('SENTINEL_STACK'))
-sys.path.insert(0, os.getenv('SQUEESAR'))
-
 logger  = send_logger()
 
 ##############################################################################
 EXAMPLE = """example:
-  create_stacksentinel_run_files.py LombokSenAT156VV.template 
+  create_squeesar_run_files.py LombokSenAT156VV.template 
 """
 
 def create_parser():
@@ -55,65 +52,68 @@ if __name__ == "__main__":
     inps = create_or_update_template(inps)
     os.chdir(inps.work_dir)
 
+    temp_list = ['run_files_SQ', 'configs_SQ']
+    _remove_directories(temp_list)
 
-    try:
-        files1 = glob.glob(inps.work_dir + '/DEM/*.wgs84')[0]
-        files2 = glob.glob(inps.work_dir + '/DEM/*.dem')[0]
-        dem_file = [files1, files2]
-        dem_file = dem_file[0]
-    except:
-        dem_file = create_or_copy_dem(work_dir=inps.work_dir,
-                                             template=inps.template,
-                                             custom_template_file=inps.custom_template_file)
+    print('inps:\n', inps)
 
-    inps.demDir = dem_file
-    script = 'stackSentinel.py'
-    extraOptions = ''
-    if inps.processingMethod == 'squeesar' or inps.processingMethod == 'ps':
-        script = 'stackSentinel_squeesar.py'
-        extraOptions = ' --processingmethod ' + inps.processingMethod
+    inps.cropbox = '"{} {} {} {}"'.format(inps.custom_template['lat_south'], inps.custom_template['lat_north'],
+                                        inps.custom_template['lon_west'], inps.custom_template['lon_east'])
 
-        
-    prefixletters = ['-slc_directory', '-orbit_directory', '-aux_directory', '-working_directory', 
-                    '-dem', '-master_date', '-num_connections', '-num_overlap_connections', 
-                     '-swath_num', '-bbox', '-exclude_dates', '-include_dates', '-azimuth_looks',
-                     '-range_looks', '-filter_strength', '-esd_coherence_threshold', '-snr_misreg_threshold', 
-                     '-unw_method', '-polarization', '-coregistration', '-workflow',
-                     '-start_date', '-stop_date', '-text_cmd']
+    command = 'stackSentinel_squeesar.py'
+
+    items = ['squeesar.plmethod','squeesar.patch_size','squeesar.range_window','squeesar.azimuth_window']
+    inpspar = []
+    defaultval = ['sequential_EMI','200','21','15']
+
+
+    for item,idef in zip(items,defaultval):
+        try:
+            inpspar.append(inps.custom_template[item])
+        except:
+            inpspar.append(idef)
+
+    inps.plmethod = inpspar[0]
+    inps.patch_size = inpspar[1]
+    inps.range_window = inpspar[2]
+    inps.azimuth_window = inpspar[3]
+    inps.slcDir = inps.work_dir+'/merged/SLC'
+
+
+    prefixletters = ['-customTemplateFile', '-slc_directory', '-working_directory',
+                     '-patchsize', '-plmethod', '-rangewindow', '-azimuthwindow', '-cropbox',
+                     '-exclude_dates', '-azimuth_looks', '-range_looks', '-unw_method',
+                     '-text_cmd']
     
-    inpsvalue = ['slcDir', 'orbitDir', 'auxDir', 'workingDir', 'demDir', 'masterDir',
-                 'numConnections', 'numOverlapConnections', 'subswath', 'boundingBox',
-                 'excludeDate', 'includeDate', 'azimuthLooks', 'rangeLooks', 'filtStrength',
-                 'esdCoherenceThreshold', 'snrThreshold', 'unwMethod', 'polarization',
-                 'coregistration', 'workflow', 'startDate', 'stopDate', 'textCmd']
+    inpsvalue = ['custom_template_file', 'slcDir', 'workingDir', 'patch_size', 'plmethod',
+                 'range_window', 'azimuth_window', 'cropbox', 'excludeDate', 'azimuthLooks', 'rangeLooks',
+                 'unwMethod', 'textCmd']
 
-    command = script + extraOptions
+
 
     for value, pref in zip(inpsvalue, prefixletters):
         keyvalue = eval('inps.' + value)
         if keyvalue is not None:
             command = command + ' -' + str(pref) + ' ' + str(keyvalue)
+
+    print(command)
     
-    out_file = 'out_stackSentinel_create_runfiles'
+    out_file = 'out_squeesar_create_runfiles'
     command = '('+command+' | tee '+out_file+'.o) 3>&1 1>&2 2>&3 | tee '+out_file+'.e'
     
     logger.log(loglevel.INFO, command)
     messageRsmas.log(command)
     
-    temp_list = ['run_files', 'configs', 'orbits']
-    _remove_directories(temp_list)
 
-    #process = subprocess.Popen( command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    #(error, output) = process.communicate()    # FA 11/18: changed order (was output,error) because of stream redirecting
-    #if process.returncode is not 0 or error or 'Traceback' in output.decode("utf-8"):
+
     status = subprocess.Popen( command, shell=True).wait()
     if status is not 0: 
         logger.log(loglevel.ERROR, 'ERROR making run_files using {}'.format(script))
         raise Exception('ERROR making run_files using {}'.format(script))
 
 
-    run_file_list = glob.glob(inps.work_dir + '/run_files/run_*')
-    with open(inps.work_dir + '/run_files_list', 'w') as run_file:
+    run_file_list = glob.glob(inps.work_dir + '/run_files_SQ/run_*')
+    with open(inps.work_dir + '/run_files_list_sq', 'w') as run_file:
         for item in run_file_list:
             run_file.writelines(item + '\n')
 
