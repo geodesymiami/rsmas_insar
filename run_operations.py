cfd2039268869b82e9fe8ceb1f9ae397f8ab959f
Joshua Zahner <joshuazahner@gmail.com>
2019-03-06 11:25:54 -0500

    Moved `check_for_operations_directories_and_initiate.py` to initiate_operations() function

--- run_operations.py
+++ run_operations.py
@@ -47,3 +47,36 @@ def command_line_parse(args):
     inps = parser.parse_args(args)
 
     return inps
+
+
+def initiate_operations():
+
+    operations_directory = os.getenv('OPERATIONS')
+    templates_directory = operations_directory + "/TEMPLATES/"
+    logs_directory = operations_directory + "/LOGS/"
+    errors_directory = operations_directory + "/ERRORS/"
+
+    if not os.path.isdir(operations_directory):
+        os.mkdir(operations_directory)
+    if not os.path.isdir(templates_directory):
+        os.mkdir(templates_directory)
+    if not os.path.isdir(logs_directory):
+        os.mkdir(logs_directory)
+        open(logs_directory + '/generate_templates.log', 'a').close()  # create empty file
+    if not os.path.isdir(errors_directory):
+        os.mkdir(errors_directory)
+    if not os.path.exists(os.getenv('OPERATIONS') + '/stored_date.date'):
+        open(os.getenv('OPERATIONS') + '/stored_date.date', 'a').close()  # create empty file
+
+
+def run_operations(args):
+
+    inps = command_line_parse(args)
+
+    if inps.restart:
+        shutil.rmtree(os.getenv('OPERATIONS'))
+        initiate_operations();
+
+if __name__ == "__main__":
+
+    run_operations()
