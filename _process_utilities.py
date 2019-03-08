2da0c3cc9641b69f596664504eb84a3ad582fb4b
David Grossman <2gotgrossman@gmail.com>
2019-03-07 23:53:31 -0500

    Removed `tryint` from _process_utilities.py

--- _process_utilities.py
+++ _process_utilities.py
@@ -416,17 +416,13 @@ def file_len(fname):
         return len(file.readlines())
 
 ##########################################################################
-def tryint(s):
-    try:
-        return int(s)
-    except ValueError:
-        return s
-     
+
+
 def alphanum_key(s):
     """ Turn a string into a list of string and number chunks.
         "z23a" -> ["z", 23, "a"]
     """
-    return [ tryint(c) for c in re.split('([0-9]+)', s) ]
+    return [ int(c) if c.isdigit() else c for c in re.split('([0-9]+)', s) ]
 
 def sort_nicely(l):
     """ Sort the given list in the way that humans expect.
