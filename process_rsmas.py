2b21dd35eb5c27c38aca7dab095006fb11d0c180
Joshua Zahner <joshuazahner@gmail.com>
2019-03-22 10:11:21 -0400

    Changed process_rsmas logic flow
    `process_rsmas` now either submits `process_rsmas` as a job file via
    `create_batch.submit_script()` or runs the remainder of the routine in
    a serial fashion, but not both. This should guarantee that, with the
    removal of the `sys.exit()` call from `create_batch`, the main body of
    `process_rsmas` is not run twice.

--- process_rsmas.py
+++ process_rsmas.py
@@ -23,6 +23,65 @@ logger_process_rsmas  = send_logger()
 ###############################################################################
 
 
+def main(start_time):
+    #########################################
+    # startssara: Getting Data
+    #########################################
+
+    # running download scripts:
+    #     download_ssara_rsmas.py $TE/template
+    #     downlaod_asfserial_rsmas.py $TE/template
+    prs.call_ssara(inps.flag_ssara, inps.custom_template_file, inps.slc_dir)
+
+    #########################################
+    # startmakerun: create run files
+    #########################################
+
+    # create or copy DEM
+    # running the script:
+    #     dem_rsmas.py $TE/template
+
+    prs.create_or_copy_dem(inps, inps.work_dir, inps.template, inps.custom_template_file)
+
+    # Check for DEM and create sentinel run files
+    # running the script:
+    #     create_stacksentinel_run_files.py $TE/template
+
+    prs.create_runfiles(inps)
+
+    #########################################
+    # startprocess: Execute run files
+    #########################################
+
+    # Running the script:
+    #    execute_stacksentinel_run_files.py $TE/template starting_run stopping_run
+    #    Example for running run 1 to 4:
+    #    execute_stacksentinel_run_files.py $TE/template 1 4
+
+    prs.process_runfiles(inps)
+
+    #########################################
+    # startpysar: running PySAR and email results
+    #########################################
+
+    if inps.processingMethod == 'squeesar':
+        # Run squeesar script:
+        #    create_squeesar_run_files.py $TE/template
+        #    execute_squeesar_run_files.py $TE/template
+        prs.process_time_series(inps)
+    else:
+        # Run PySAR script:
+        #    pysarApp.py $TE/template
+        prs.run_pysar(inps, start_time)
+
+    # Run ingest insarmaps script and email results
+    #    ingest_insarmaps.py $TE/template
+
+    prs.run_ingest_insarmaps(inps)
+
+    logger_process_rsmas.log(loglevel.INFO, 'End of process_rsmas')
+
+
 if __name__ == "__main__":
 
     #########################################
@@ -65,61 +124,5 @@ if __name__ == "__main__":
         job_file_name = 'process_rsmas'
         wall_time = '48:00'
         cb.submit_script(inps.project_name, job_file_name, sys.argv[:], inps.work_dir, wall_time)
-
-    #########################################
-    # startssara: Getting Data
-    #########################################
-
-    # running download scripts:
-    #     download_ssara_rsmas.py $TE/template
-    #     downlaod_asfserial_rsmas.py $TE/template
-    prs.call_ssara(inps.flag_ssara, inps.custom_template_file, inps.slc_dir)
-
-    #########################################
-    # startmakerun: create run files
-    #########################################
-
-    # create or copy DEM
-    # running the script:
-    #     dem_rsmas.py $TE/template
-
-    prs.create_or_copy_dem(inps, inps.work_dir, inps.template, inps.custom_template_file)
-
-    # Check for DEM and create sentinel run files
-    # running the script:
-    #     create_stacksentinel_run_files.py $TE/template
-
-    prs.create_runfiles(inps)
-
-    #########################################
-    # startprocess: Execute run files
-    #########################################
-
-    # Running the script:
-    #    execute_stacksentinel_run_files.py $TE/template starting_run stopping_run
-    #    Example for running run 1 to 4:
-    #    execute_stacksentinel_run_files.py $TE/template 1 4
-
-    prs.process_runfiles(inps)
-
-    #########################################
-    # startpysar: running PySAR and email results
-    #########################################
-
-
-    if inps.processingMethod == 'squeesar' :
-        # Run squeesar script:
-        #    create_squeesar_run_files.py $TE/template
-        #    execute_squeesar_run_files.py $TE/template
-        prs.process_time_series(inps)
     else:
-        # Run PySAR script:
-        #    pysarApp.py $TE/template
-        prs.run_pysar(inps, start_time)
-
-    # Run ingest insarmaps script and email results
-    #    ingest_insarmaps.py $TE/template
-
-    prs.run_ingest_insarmaps(inps)
-
-    logger_process_rsmas.log(loglevel.INFO, 'End of process_rsmas')
+        main(start_time)
