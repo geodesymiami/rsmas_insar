fc6bd619cb6c2e6b213d2b6d09409484022d88bf
Joshua Zahner <joshuazahner@gmail.com>
2019-03-25 11:29:22 -0400

    Moved initialization routine into main function

--- process_rsmas.py
+++ process_rsmas.py
@@ -13,7 +13,7 @@ import os
 import sys
 import time
 import messageRsmas
-from _process_utilities  import get_work_directory, get_project_name, send_logger, _remove_directories
+from _process_utilities import get_work_directory, get_project_name, send_logger, _remove_directories
 import _processSteps as prs
 import create_batch as cb
 from rsmas_logging import loglevel
@@ -23,7 +23,39 @@ logger_process_rsmas  = send_logger()
 ###############################################################################
 
 
-def main(start_time):
+def main(inps):
+
+    #########################################
+    # Initiation
+    #########################################
+    start_time = time.time()
+
+    inps.project_name = get_project_name(inps.custom_template_file)
+    inps.work_dir = get_work_directory(None, inps.project_name)
+    inps.slc_dir = os.path.join(inps.work_dir, 'SLC')
+
+    if inps.remove_project_dir:
+        _remove_directories(directories_to_delete=[inps.work_dir])
+
+    if not os.path.isdir(inps.work_dir):
+        os.makedirs(inps.work_dir)
+    os.chdir(inps.work_dir)
+
+    #  Read and update template file:
+    inps = prs.create_or_update_template(inps)
+    print(inps)
+    if not inps.processingMethod or inps.workflow == 'interferogram':
+        inps.processingMethod = 'sbas'
+
+    if not os.path.isdir(inps.slc_dir):
+        os.makedirs(inps.slc_dir)
+
+    command_line = os.path.basename(sys.argv[0]) + ' ' + ' '.join(sys.argv[1:])
+    logger_process_rsmas.log(loglevel.INFO, '##### NEW RUN #####')
+    logger_process_rsmas.log(loglevel.INFO, 'process_rsmas.py ' + command_line)
+    messageRsmas.log('##### NEW RUN #####')
+    messageRsmas.log(command_line)
+
     #########################################
     # startssara: Getting Data
     #########################################
@@ -84,38 +116,7 @@ def main(start_time):
 
 if __name__ == "__main__":
 
-    #########################################
-    # Initiation
-    #########################################
-
-    start_time = time.time()
     inps = prs.command_line_parse()
-
-    inps.project_name = get_project_name(inps.custom_template_file)
-    inps.work_dir = get_work_directory(None, inps.project_name)
-    inps.slc_dir = os.path.join(inps.work_dir,'SLC')
-
-    if inps.remove_project_dir:
-        _remove_directories(directories_to_delete=[inps.work_dir])
-
-    if not os.path.isdir(inps.work_dir):
-        os.makedirs(inps.work_dir)
-    os.chdir(inps.work_dir)
-
-    #  Read and update template file:
-    inps = prs.create_or_update_template(inps)
-    print(inps)
-    if not inps.processingMethod or inps.workflow=='interferogram':
-        inps.processingMethod='sbas'
-
-    if not os.path.isdir(inps.slc_dir):
-        os.makedirs(inps.slc_dir)
-
-    command_line = os.path.basename(sys.argv[0]) + ' ' + ' '.join(sys.argv[1:])
-    logger_process_rsmas.log(loglevel.INFO, '##### NEW RUN #####')
-    logger_process_rsmas.log(loglevel.INFO, 'process_rsmas.py ' + command_line)
-    messageRsmas.log('##### NEW RUN #####')
-    messageRsmas.log(command_line)
     
     #########################################
     # Submit job
@@ -123,6 +124,10 @@ if __name__ == "__main__":
     if inps.submit_flag:
         job_file_name = 'process_rsmas'
         wall_time = '48:00'
-        cb.submit_script(inps.project_name, job_file_name, sys.argv[:], inps.work_dir, wall_time)
+
+        project_name = get_project_name(inps.custom_template_file)
+        work_dir = get_work_directory(None, inps.project_name)
+
+        cb.submit_script(project_name, job_file_name, sys.argv[:], work_dir, wall_time)
     else:
-        main(start_time)
+        main(inps)
