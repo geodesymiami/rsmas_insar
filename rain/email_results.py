1c5cdea06c5d7f899fbf8294823d7b5c581bf3d7
saramirzaee <sara.mirzaee@rsmas.miami.edu>
2019-04-10 17:53:22 -0400

    Modified to email both insarmaps and Pysar results

--- rain/email_results.py
+++ rain/email_results.py
@@ -26,99 +26,103 @@ def command_line_parse(iargs=None):
 
 def create_parser():
     """ Creates command line argument parser object. """
-    parser = argparse.ArgumentParser(description='Email pysar results',
+    parser = argparse.ArgumentParser(description='Email results (by default pysar results)',
                                      formatter_class=argparse.RawTextHelpFormatter, epilog=EXAMPLE)
     parser.add_argument('template_file', help='template file containing ssaraopt field')
-    #parser.add_argument('template_file', help='template file containing ssaraopt field', nargs='?')
+    parser.add_argument('--insarmap', action='store_true', dest='insarmap', default=False,
+                    help = 'Email insarmap results')
     return parser
 
-###############################################################################
-def generate_file_lists(number_of_files=2):
 
-    if number_of_files is 2:
-        file_list1 = ['velocity.png',\
-                 'avgSpatialCoherence.png',\
-                 'temporalCoherence.png',\
-                 'coherence*.png',\
-                 'unwrapPhase*.png',\
-                 #'mask.png',\
-                 'maskTempCoh.png',\
-                 'demRadar_error.png',\
-                 'geo_velocity.png',\
-                 'rms_timeseriesResidual_quadratic.pdf',\
-                 'CoherenceHistory.pdf',\
-                 'CoherenceMatrix.pdf',\
-                 'bl_list.txt',\
-                 'Network.pdf',\
-                 'geo_velocity_masked.kmz']
+###################################################
 
-        file_list2 = ['timeseries*.png']
-    
-        file_lists = [file_list1, file_list2]
+def email_insarmaps_results(custom_template):
+    """ email link to insarmaps.miami.edu """
 
-    elif number_of_files is 3:
-        file_list1 = ['velocity.png',\
-                 #'avgSpatialCoherence.png',\
-                 #'temporalCoherence.png',\
-                 #'coherence*.png']
-                 ]
-
-        #file_list2 = [ 'unwrapPhase*.png',\
-        file_list2 = ['unwrapPhase_wrap_*.png', 'geo_velocity_masked.kmz' ]                    
-                 #'maskTempCoh.png',\
-                 #'demRadar_error.png',\
-                 #'geo_velocity.png',\
-                 #'rms_timeseriesResidual_quadratic.pdf',\
-                 #'CoherenceHistory.pdf',\
-                 #'CoherenceMatrix.pdf',\
-                 #'bl_list.txt',\
-                 #'Network.pdf',\
-                 # 'geo_velocity_masked.kmz']
-
-        file_list3 = ['timeseries*.png']
-    
-        file_lists = [file_list1, file_list2, file_list3]
-
-    else:
-        raise Exception('ERROR number_of_files of xx not supported')
-        
-    return file_lists
-
-###########################################################################################
-def main(iargs=None, text_string='email pysar results'):
-    """ email pysar results """
-
-    template_file = glob.glob('PYSAR/INPUTS/*.template')[0]
-    email_list = Template(template_file).get_options()['email_pysar']
-    if not  email_list:
+    if 'email_insarmaps' not in custom_template:
         return
 
-    if os.path.isdir('PYSAR/PIC'):
-       prefix='PYSAR/PIC'
+    cwd = os.getcwd()
 
-    file_lists = generate_file_lists(number_of_files=3)
+    hdfeos_file = glob.glob('./PYSAR/S1*.he5')
+    hdfeos_file = hdfeos_file[0]
+    hdfeos_name = os.path.splitext(os.path.basename(hdfeos_file))[0]
+
+    textStr = 'http://insarmaps.miami.edu/start/-0.008/-78.0/7"\?"startDataset=' + hdfeos_name
+
+    mailCmd = 'echo \"' + textStr + '\" | mail -s Miami_InSAR_results:_' + os.path.basename(cwd) + ' ' + \
+              custom_template['email_insarmaps']
+    command = 'ssh pegasus.ccs.miami.edu \" ' + mailCmd + '\"'
+
+    print(command)
+    status = subprocess.Popen(command, shell=True).wait()
+    if status is not 0:
+        sys.exit('Error in email_insarmaps_results')
+
+    return
+
+
+def email_pysar_results(custom_template):
+    """ email pysar results """
+
+    textStr = 'email pysar results'
+
+    if 'email_pysar' not in custom_template:
+        return
+
+    cwd = os.getcwd()
+
+    fileList1 = ['velocity.png', 'avgSpatialCoherence.png', 'temporalCoherence.png', 'maskTempCoh.png', 'mask.png',
+                 'demRadar_error.png', 'velocityStd.png', 'geo_velocity.png', 'coherence*.png', 'unwrapPhase*.png',
+                 'rms_timeseriesResidual_quadratic.pdf', 'CoherenceHistory.pdf', 'CoherenceMatrix.pdf', 'bl_list.txt',
+                 'Network.pdf', 'geo_velocity_masked.kmz']
+
+    fileList2 = ['timeseries*.png','geo_timeseries*.png']
+
+    if os.path.isdir('PYSAR/PIC'):
+        prefix = 'PYSAR/PIC'
+
+    template_file = glob.glob('PYSAR/INPUTS/*.template')[0]
 
     i = 0
-    cwd = os.getcwd()
-    #import pdb; pdb.set_trace()
-    for file_list in file_lists:
-       print ('###')
-       attachment_string = ''
-       i = i + 1
-       for fname in file_list:
-           files = glob.glob(prefix+'/'+fname)
-           for file in files:
-               attachment_string = attachment_string+' -a '+file
+    for fileList in [fileList1, fileList2]:
+        attachmentStr = ''
+        i = i + 1
+        for fname in fileList:
+            fList = glob.glob(prefix + '/' + fname)
+            for fileName in fList:
+                attachmentStr = attachmentStr + ' -a ' + fileName
 
-       if i==1 and len(template_file)>0:
-          attachment_string = attachment_string+' -a '+template_file
+        if i == 1 and len(template_file) > 0:
+            attachmentStr = attachmentStr + ' -a ' + template_file
 
-       mail_command = 'echo \"'+text_string+'\" | mail -s '+cwd+' '+attachment_string+' '+ email_list
-       command = 'ssh pegasus.ccs.miami.edu \"cd '+cwd+'; '+mail_command+'\"'
-       print(command)
-       status = subprocess.Popen(command, shell=True).wait()
-       if status is not 0:
-          sys.exit('Error in email_pysar_results')
+        mailCmd = 'echo \"' + textStr + '\" | mail -s ' + cwd + ' ' + attachmentStr + ' ' + custom_template[
+            'email_pysar']
+        command = 'ssh pegasus.ccs.miami.edu \"cd ' + cwd + '; ' + mailCmd + '\"'
+        print(command)
+        status = subprocess.Popen(command, shell=True).wait()
+        if status is not 0:
+            sys.exit('Error in email_pysar_results')
+
+    return
+
+###########################################################################################
+def main(iargs=None):
+    """ email pysar or insarmap results """
+
+    inps = command_line_parse(iargs)
+
+    custom_template = readfile.read_template(inps.template_file)
+
+    if inps.insarmap:
+        email_insarmaps_results(custom_template)
+
+        if int(custom_template['cleanopt']) == 4:
+            cleanlist = clean_list()
+            _remove_directories(cleanlist[4])
+
+    else:
+        email_pysar_results(custom_template)
 
 
 ###########################################################################################
