382959ce418656e44a4a373b813037ee5e4d6c70
saramirzaee <sara.mirzaee@rsmas.miami.edu>
2019-04-10 17:52:34 -0400

    New created

---
+++ rain/execute_pre_runfiles.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+########################
+# Author: Sara Mirzaee
+#######################
+
+import os
+import argparse
+import subprocess
+from rain.utils.process_utilities import get_project_name, remove_zero_size_or_length_files
+
+
+##############################################################################
+EXAMPLE = """example:
+  execute_pre_runfiles.py LombokSenAT156VV.template 
+"""
+
+
+def create_parser():
+    """ Creates command line argument parser object. """
+
+    parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter, epilog=EXAMPLE)
+    parser.add_argument('-v', '--version', action='version', version='%(prog)s 0.1')
+    parser.add_argument('custom_template_file', nargs='?',
+                        help='custom template with option settings.\n')
+    parser.add_argument('start', nargs='?', type=int,
+                        help='starting run file number (default = 1).\n')
+    parser.add_argument('stop', nargs='?', type=int,
+                        help='stopping run file number.\n')
+    return parser
+
+
+def command_line_parse(iargs=None):
+    """ Parses command line agurments into inps variable. """
+
+    parser = create_parser()
+    inps = parser.parse_args(args=iargs)
+
+    return inps
+
+
+def get_run_files(inps):
+    """ Reads pre processing runfiles to a list. """
+
+    runfiles = os.path.join(inps.work_dir, 'pre_run_files_list')
+    run_file_list = []
+    with open(runfiles, 'r') as f:
+        new_f = f.readlines()
+        for line in new_f:
+            run_file_list.append('pre_run_files/' + line.split('/')[-1][:-1])
+
+    return run_file_list
+
+
+def set_memory_defaults():
+    """ Sets an optimized memory value for each job. """
+
+    memoryuse = {'download_data': '3700',
+                 'download_dem': '3700'}
+
+    return memoryuse
+
+
+def submit_run_jobs(run_file_list, cwd, memoryuse):
+    """ Submits stackSentinel runfile jobs. """
+
+    for item in run_file_list:
+        item_memory = '_'
+        item_memory = item_memory.join(item.split('_')[3::])
+        try:
+            memorymax = str(memoryuse[item_memory])
+        except:
+            memorymax = '3700'
+
+        if os.getenv('QUEUENAME') == 'debug':
+            walltimelimit = '0:30'
+        else:
+            walltimelimit = '4:00'
+
+        queuename = os.getenv('QUEUENAME')
+
+        cmd = 'create_batch.py ' + cwd + '/' + item + ' --memory=' + memorymax + ' --walltime=' + walltimelimit + \
+              ' --queuename ' + queuename + ' --outdir "pre_run_files"'
+
+        print('command:', cmd)
+        status = subprocess.Popen(cmd, shell=True).wait()
+        if status is not 0:
+            raise Exception('ERROR submitting {} using create_batch.py'.format(item))
+
+        job_folder = cwd + '/' + item + '_out_jobs'
+        print('jobfolder:', job_folder)
+
+        remove_zero_size_or_length_files(directory='pre_run_files')
+
+        if not os.path.isdir(job_folder):
+            os.makedirs(job_folder)
+
+        mvlist = ['*.e ', '*.o ', '*.job ']
+        for mvitem in mvlist:
+            cmd = 'mv ' + cwd + '/pre_run_files/' + mvitem + job_folder
+            print('move command:', cmd)
+            os.system(cmd)
+
+    return
+
+
+##############################################################################
+
+def main(iargs=None):
+    inps = command_line_parse(iargs)
+
+    inps.project_name = get_project_name(inps.custom_template_file)
+    inps.work_dir = os.getenv('SCRATCHDIR') + '/' + inps.project_name
+
+    run_file_list = get_run_files(inps)
+
+    if inps.start is None:
+        inps.start = 1
+    if inps.stop is None:
+        inps.stop = len(run_file_list)
+
+    memoryuse = set_memory_defaults()
+
+    submit_run_jobs(run_file_list[inps.start - 1:inps.stop], inps.work_dir, memoryuse)
+    return None
+
+
+###########################################################################################
+
+if __name__ == "__main__":
+    main()
---
+++ rain/objects/stack_rsmas.py
@@ -0,0 +1,316 @@
+#!/usr/bin/env python3
+########################
+# Author: Sara Mirzaee
+#######################
+
+import os
+
+noMCF = 'False'
+defoMax = '2'
+maxNodes = 72
+
+
+###################################
+
+class config(object):
+    """
+       A class representing the config file
+    """
+
+    def __init__(self, config_path, outname):
+        if not os.path.exists(config_path):
+            os.makedirs(config_path)
+        self.f = open(outname, 'w')
+        self.f.write('[Common]' + '\n')
+        self.f.write('')
+        self.f.write('##########################' + '\n')
+
+    def configure(self, inps):
+        for k in inps.__dict__.keys():
+            setattr(self, k, inps.__dict__[k])
+        self.plot = 'False'
+        self.misreg_az = None
+        self.misreg_rng = None
+        self.multilook_tool = None
+        self.no_data_value = None
+        self.cleanup = None  ###SSS 7/2018: clean-up fine*int, if specified.
+
+    def crop_sentinel(self, function):
+        self.f.write('###################################' + '\n')
+        self.f.write(function + '\n')
+        self.f.write('crop_sentinel : ' + '\n')
+        self.f.write('input : ' + self.input + '\n')
+        self.f.write('output : ' + self.output + '\n')
+        self.f.write('bbox : ' + self.bbox + '\n')
+        self.f.write('multilook : ' + self.multi_look + '\n')
+        self.f.write('range_looks : ' + self.rangeLooks + '\n')
+        self.f.write('azimuth_looks : ' + self.azimuthLooks + '\n')
+        self.f.write('multilook_tool : ' + self.multilook_tool + '\n')
+
+    def create_patch(self, function):
+        self.f.write('###################################' + '\n')
+        self.f.write(function + '\n')
+        self.f.write('create_patch : ' + '\n')
+        self.f.write('slc_dir : ' + self.slcDir + '\n')
+        self.f.write('squeesar_dir : ' + self.sqDir + '\n')
+        self.f.write('patch_size : ' + self.patchSize + '\n')
+        self.f.write('range_window : ' + self.rangeWindow + '\n')
+        self.f.write('azimuth_window : ' + self.azimuthWindow + '\n')
+
+    def phase_link(self, function):
+        self.f.write('###################################' + '\n')
+        self.f.write(function + '\n')
+        self.f.write('PSQ_sentinel : ' + '\n')
+        self.f.write('patch_dir : ' + self.patchDir + '\n')
+        self.f.write('range_window : ' + self.rangeWindow + '\n')
+        self.f.write('azimuth_window : ' + self.azimuthWindow + '\n')
+        self.f.write('plmethod : ' + self.plmethod + '\n')
+
+    def generate_igram(self, function):
+        self.f.write('###################################' + '\n')
+        self.f.write(function + '\n')
+        self.f.write('generate_ifgram_sq : ' + '\n')
+        self.f.write('squeesar_dir : ' + self.sqDir + '\n')
+        self.f.write('ifg_dir : ' + self.ifgDir + '\n')
+        self.f.write('ifg_index : ' + self.ifgIndex + '\n')
+        self.f.write('range_window : ' + self.rangeWindow + '\n')
+        self.f.write('azimuth_window : ' + self.azimuthWindow + '\n')
+        self.f.write('acquisition_number : ' + self.acq_num + '\n')
+        self.f.write('range_looks : ' + self.rangeLooks + '\n')
+        self.f.write('azimuth_looks : ' + self.azimuthLooks + '\n')
+        if 'geom_master' in self.ifgDir:
+            self.f.write('plmethod : ' + self.plmethod + '\n')
+
+    def unwrap(self, function):
+        self.f.write('###################################' + '\n')
+        self.f.write(function + '\n')
+        self.f.write('unwrap : ' + '\n')
+        self.f.write('ifg : ' + self.ifgName + '\n')
+        self.f.write('unw : ' + self.unwName + '\n')
+        self.f.write('coh : ' + self.cohName + '\n')
+        self.f.write('nomcf : ' + self.noMCF + '\n')
+        self.f.write('master : ' + self.master + '\n')
+        # self.f.write('defomax : ' + self.defoMax + '\n')
+        self.f.write('alks : ' + self.rangeLooks + '\n')
+        self.f.write('rlks : ' + self.azimuthLooks + '\n')
+        self.f.write('method : ' + self.unwMethod + '\n')
+
+    def unwrapSnaphu(self, function):
+        self.f.write('###################################' + '\n')
+        self.f.write(function + '\n')
+        self.f.write('unwrapSnaphu : ' + '\n')
+        self.f.write('ifg : ' + self.ifgName + '\n')
+        self.f.write('unw : ' + self.unwName + '\n')
+        self.f.write('coh : ' + self.cohName + '\n')
+        self.f.write('nomcf : ' + self.noMCF + '\n')
+        self.f.write('master : ' + self.master + '\n')
+        # self.f.write('defomax : ' + self.defoMax + '\n')
+        self.f.write('alks : ' + self.rangeLooks + '\n')
+        self.f.write('rlks : ' + self.azimuthLooks + '\n')
+
+    def finalize(self):
+        self.f.close()
+
+
+################################################
+
+class pre_run(object):
+    """
+       A class representing a run which may contain several functions
+    """
+
+    # def __init__(self):
+
+    def configure(self, inps, runName):
+        for k in inps.__dict__.keys():
+            setattr(self, k, inps.__dict__[k])
+        self.runDir = os.path.join(self.work_dir, 'pre_run_files')
+        if not os.path.exists(self.runDir):
+            os.makedirs(self.runDir)
+
+        self.run_outname = os.path.join(self.runDir, runName)
+        print ('writing ', self.run_outname)
+
+        self.config_path = os.path.join(self.work_dir, 'configs')
+
+        self.runf = open(self.run_outname, 'w')
+
+    def downloadData(self, inps):
+        # self.runf.write(self.text_cmd + 'download_rsmas.py ' + inps.customTemplateFile + '\n')
+        self.runf.write('download_rsmas.py ' + inps.customTemplateFile + '\n')
+
+    def creatOrCopyDEM(self, inps):
+        # self.runf.write(self.text_cmd + 'dem_rsmas.py ' + inps.customTemplateFile + '\n')
+        self.runf.write('dem_rsmas.py ' + inps.customTemplateFile + '\n')
+
+    def finalize(self):
+        self.runf.close()
+
+
+################################################
+
+class post_run(object):
+    """
+       A class representing a run which may contain several functions
+    """
+
+    # def __init__(self):
+
+    def configure_run(self, inps, runName):
+        for k in inps.__dict__.keys():
+            setattr(self, k, inps.__dict__[k])
+        self.runDir = os.path.join(self.work_dir, 'post_run_files')
+
+        self.slcDir = os.path.join(self.work_dir, 'merged/SLC')
+
+        if not os.path.exists(self.runDir):
+            os.makedirs(self.runDir)
+
+        self.run_outname = os.path.join(self.runDir, runName)
+        print ('writing ', self.run_outname)
+
+        self.config_path = os.path.join(self.work_dir, 'configs')
+
+        self.runf = open(self.run_outname, 'w')
+
+    def cropMergedSlc(self, acquisitions, inps):
+        for slc in acquisitions:
+            cropDir = os.path.join(self.work_dir, 'merged/SLC/' + slc)
+            configName = os.path.join(self.config_path, 'config_crop_' + slc)
+            configObj = config(self.config_path, configName)
+            configObj.configure(self)
+            configObj.input = os.path.join(cropDir, slc + '.slc.full')
+            configObj.output = os.path.join(cropDir, slc + '.slc')
+            configObj.bbox = inps.bbox_rdr
+            configObj.multi_look = 'False'
+            configObj.rangeLooks = inps.rangeLooks
+            configObj.azimuthLooks = inps.azimuthLooks
+            configObj.multilook_tool = 'gdal'
+            configObj.crop_sentinel('[Function-1]')
+            configObj.finalize()
+            self.runf.write(self.text_cmd + 'wrapper_rsmas.py -c ' + configName + '\n')
+
+        list_geo = ['lat', 'lon', 'los', 'hgt', 'shadowMask', 'incLocal']
+        multiookToolDict = {'lat*rdr': 'gdal', 'lon*rdr': 'gdal', 'los*rdr': 'gdal', 'hgt*rdr': "gdal",
+                            'shadowMask*rdr': "isce", 'incLocal*rdr': "gdal"}
+        for item in list_geo:
+            pattern = item + '*rdr'
+            geoDir = os.path.join(self.work_dir, 'merged/geom_master/')
+            configName = os.path.join(self.config_path, 'config_crop_' + item)
+            configObj = config(self.config_path, configName)
+            configObj.configure(self)
+            configObj.input = os.path.join(geoDir, item + '.rdr.full')
+            configObj.output = os.path.join(geoDir, item + '.rdr')
+            configObj.bbox = inps.bbox_rdr
+            configObj.multi_look = 'False'
+            configObj.rangeLooks = inps.rangeLooks
+            configObj.azimuthLooks = inps.azimuthLooks
+            configObj.multilook_tool = multiookToolDict[pattern]
+            configObj.crop_sentinel('[Function-1]')
+            configObj.finalize()
+            self.runf.write(self.text_cmd + 'wrapper_rsmas.py -c ' + configName + '\n')
+
+    def createPatch(self, inps):
+        configName = os.path.join(self.config_path, 'config_create_patch')
+        configObj = config(self.config_path, configName)
+        configObj.configure(self)
+        configObj.slcDir = self.slcDir
+        configObj.sqDir = inps.squeesar_dir
+        configObj.patchSize = inps.patch_size
+        configObj.rangeWindow = inps.range_window
+        configObj.azimuthWindow = inps.azimuth_window
+        configObj.create_patch('[Function-1]')
+        configObj.finalize()
+        self.runf.write(self.text_cmd + 'wrapper_rsmas.py -c ' + configName + '\n')
+
+    def phaseLinking(self, inps):
+
+        for patch in inps.patch_list:
+            configName = os.path.join(self.config_path, 'config_phase_link_PATCH' + patch)
+            configObj = config(self.config_path, configName)
+            configObj.configure(self)
+            configObj.patchDir = os.path.join(inps.squeesar_dir, 'PATCH' + patch)
+            configObj.rangeWindow = inps.range_window
+            configObj.azimuthWindow = inps.azimuth_window
+            configObj.plmethod = inps.plmethod
+            configObj.phase_link('[Function-1]')
+            configObj.finalize()
+            self.runf.write(self.text_cmd + 'wrapper_rsmas.py -c ' + configName + '\n')
+
+    def generateIfg(self, inps, acquisitions):
+        ifgram_dir = os.path.dirname(self.slcDir) + '/interferograms'
+        if not os.path.isdir(ifgram_dir):
+            os.mkdir(ifgram_dir)
+        index = 0
+        for ifg in acquisitions[1::]:
+            index += 1
+            configName = os.path.join(self.config_path, 'config_generate_ifgram_{}_{}'.format(acquisitions[0], ifg))
+            configObj = config(self.config_path, configName)
+            configObj.configure(self)
+            configObj.sqDir = inps.squeesar_dir
+            configObj.ifgDir = os.path.join(ifgram_dir, '{}_{}'.format(acquisitions[0], ifg))
+            configObj.ifgIndex = str(index)
+            configObj.rangeWindow = inps.range_window
+            configObj.azimuthWindow = inps.azimuth_window
+            configObj.acq_num = str(len(acquisitions))
+            configObj.rangeLooks = inps.rangeLooks
+            configObj.azimuthLooks = inps.azimuthLooks
+            configObj.generate_igram('[Function-1]')
+            configObj.finalize()
+            self.runf.write(self.text_cmd + 'wrapper_rsmas.py -c ' + configName + '\n')
+        configName = os.path.join(self.config_path, 'config_generate_quality_map')
+        configObj = config(self.config_path, configName)
+        configObj.configure(self)
+        configObj.sqDir = inps.squeesar_dir
+        configObj.ifgDir = inps.geo_master_dir
+        configObj.ifgIndex = str(0)
+        configObj.rangeWindow = inps.range_window
+        configObj.azimuthWindow = inps.azimuth_window
+        configObj.acq_num = str(len(acquisitions))
+        configObj.rangeLooks = inps.rangeLooks
+        configObj.azimuthLooks = inps.azimuthLooks
+        configObj.plmethod = inps.plmethod
+        configObj.generate_igram('[Function-1]')
+        configObj.finalize()
+        self.runf.write(self.text_cmd + 'wrapper_rsmas.py -c ' + configName + '\n')
+
+    def unwrap(self, inps, pairs):
+        for pair in pairs:
+            master = pair[0]
+            slave = pair[1]
+            mergedDir = os.path.join(self.work_dir, 'merged/interferograms/' + master + '_' + slave)
+            configName = os.path.join(self.config_path, 'config_igram_unw_' + master + '_' + slave)
+            configObj = config(self.config_path, configName)
+            configObj.configure(self)
+            configObj.ifgName = os.path.join(mergedDir, 'filt_fine.int')
+            configObj.cohName = os.path.join(mergedDir, 'filt_fine.cor')
+            configObj.unwName = os.path.join(mergedDir, 'filt_fine.unw')
+            configObj.noMCF = noMCF
+            configObj.master = os.path.join(self.work_dir, 'master')
+            configObj.defoMax = defoMax
+            configObj.unwMethod = inps.unwMethod
+            configObj.unwrap('[Function-1]')
+            configObj.finalize()
+            self.runf.write(self.text_cmd + 'wrapper_rsmas.py -c ' + configName + '\n')
+
+    def pysarCorrections(self, inps):
+        self.runf.write(self.text_cmd + 'timeseries_corrections.py ' + inps.customTemplateFile + '\n')
+
+    def pysarSB(self, inps):
+        self.runf.write(self.text_cmd + 'pysarApp.py ' + inps.customTemplateFile + '\n')
+
+    def emailPySAR(self, inps):
+        self.runf.write(self.text_cmd + 'email_results.py ' + inps.customTemplateFile + '\n')
+
+    def ingestInsarmaps(self, inps):
+        self.runf.write(self.text_cmd + 'ingest_insarmaps.py ' + inps.customTemplateFile + '\n')
+
+    def emailInsarmaps(self, inps):
+        self.runf.write(self.text_cmd + 'email_results.py ' + inps.customTemplateFile + ' --insarmap\n')
+
+    def finalize(self):
+        self.runf.close()
+
+#######################################################
+
+
