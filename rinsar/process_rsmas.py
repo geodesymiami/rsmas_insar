c15a604e6bdd44cbb53c16d1238b9538b465ea53
saramirzaee <sara.mirzaee@rsmas.miami.edu>
2019-04-11 14:26:09 -0400

    Added submit job

--- rinsar/process_rsmas.py
+++ rinsar/process_rsmas.py
@@ -14,20 +14,34 @@ import sys
 import time
 from rinsar.objects import messageRsmas
 from rinsar.utils.process_steps import RsmasInsar, command_line_parse
-
+from rinsar.utils.process_utilities import get_work_directory, get_project_name
+import rinsar.create_batch as cb
 ###############################################################################
 
 def main(iargs=None):
     start_time = time.time()
     inps = command_line_parse(iargs)
 
-    command_line = os.path.basename(sys.argv[0]) + ' ' + ' '.join(sys.argv[1:])
-    messageRsmas.log('##### NEW RUN #####')
-    messageRsmas.log(command_line)
+    #########################################
+    # Submit job
+    #########################################
+    if inps.submit_flag:
+        job_file_name = 'process_rsmas'
+        wall_time = '48:00'
 
-    objInsar = RsmasInsar(inps.customTemplateFile, inps.work_dir)
-    objInsar.startup()
-    objInsar.run(steps=inps.runSteps)
+        project_name = get_project_name(inps.customTemplateFile)
+        work_dir = get_work_directory(None, project_name)
+
+        job = cb.submit_script(project_name, job_file_name, sys.argv[:], work_dir, wall_time)
+
+    else:
+        command_line = os.path.basename(sys.argv[0]) + ' ' + ' '.join(sys.argv[1:])
+        messageRsmas.log('##### NEW RUN #####')
+        messageRsmas.log(command_line)
+
+        objInsar = RsmasInsar(inps.customTemplateFile, inps.work_dir)
+        objInsar.startup()
+        objInsar.run(steps=inps.runSteps)
 
     # Timing
     m, s = divmod(time.time() - start_time, 60)
