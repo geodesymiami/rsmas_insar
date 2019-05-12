a05dcf87ffbafbeca416b5d5b7be51cfd3193db4
Sara Mirzaee <sxm1611@login4.pegasus.edu>
2019-05-12 12:13:17 -0400

    bug fixed

--- rinsar/ifgramStack_to_ifgram_and_coherence.py
+++ rinsar/ifgramStack_to_ifgram_and_coherence.py
@@ -49,6 +49,7 @@ def create_parser():
 
     return parser
 
+
 ###############################################################################
 
 def main(iargs=None):
@@ -77,7 +78,7 @@ def main(iargs=None):
 
     # geocode ifgramStack
     geo_file = os.path.dirname( os.path.dirname(file)) + '/GEOCODE/geo_' + os.path.basename(file)
-    lookup_file =  os.path.dirname( os.path.dirname(file)) + '/INPUTS/geometryRadar.h5'
+    lookup_file = os.path.dirname( os.path.dirname(file)) + '/INPUTS/geometryRadar.h5'
     template_file = os.path.dirname(os.path.dirname(file)) + '/pysarApp_template.txt'
     arg_string = file + ' -t ' + template_file + ' -l ' + lookup_file + ' -o ' + geo_file
     print('geocode.py', arg_string)
@@ -89,12 +90,13 @@ def main(iargs=None):
     date12_list = obj.get_date12_list()
     dummy_data, atr = readfile.read(geo_file)
 
-    #for i in  range(2):
-    for i in  range(len(date12_list)):
+    #for i in range(2):
+    for i in range(len(date12_list)):
         date_str = date12_list[i]
         print('Working on ... ' + date_str)
-        data_coh = readfile.read(geo_file, datasetName='coherence-' + date_str)[0]
-        data_unw = readfile.read(geo_file, datasetName='unwrapPhase-' + date_str)[0]
+        data_coh = readfile.read(file, datasetName='coherence-' + date_str)[0]
+        data_unw = readfile.read(file, datasetName='unwrapPhase-' + date_str)[0]
+
 
         fname_coh = out_dir + '/coherence_' + date_str + '.tif'
         fname_unw = out_dir + '/interferogram_' + date_str + '.tif'
@@ -111,7 +113,7 @@ def create_geotiff (obj, data, outfile, type):
     originY = float(obj.get_metadata()['Y_FIRST'])
     pixelHeight = float(obj.get_metadata()['Y_STEP'])
     gt = [originX, pixelWidth, 0, originY, 0, pixelHeight]
-    
+
     driver = gdal.GetDriverByName('GTiff')
     ds = driver.Create(outfile, data.shape[1], data.shape[0], 1, gdal.GDT_Float32, )
 
@@ -120,7 +122,9 @@ def create_geotiff (obj, data, outfile, type):
     srs.ImportFromEPSG(4326)
     ds.SetProjection(srs.ExportToWkt())
     ds.SetGeoTransform(gt)
-    ds.SetMetadata(Metadata)
+    ds.SetMetadata(obj.get_metadata())
+
+
     ## TODO: Need to add metadata data_content: 'coherence' (use variable type)
     ## NEED HELP: I could not figure it out
 
