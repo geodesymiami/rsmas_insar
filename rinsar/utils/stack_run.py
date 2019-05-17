40b0c1ba5fe10d3e16abb2cbe66fd2df693f95bf
Sara Mirzaee <sxm1611@login4.pegasus.edu>
2019-05-17 18:13:13 -0400

    updated inps and made hazard products optional

--- rinsar/utils/stack_run.py
+++ rinsar/utils/stack_run.py
@@ -8,12 +8,13 @@ import os
 import sys
 import numpy as np
 import datetime
+from argparse import Namespace
 import shutil
 from stackSentinel import *
 from Stack import sentinelSLC
 from matplotlib.path import Path as Path
 from rinsar.objects.stack_rsmas import rsmasRun
-from rinsar.utils.process_utilities import get_slc_list, make_run_list
+from rinsar.utils.process_utilities import make_run_list
 from rinsar.objects.auto_defaults import PathFind
 
 pathObj = PathFind()
@@ -29,16 +30,14 @@ def run_download(inps):
             shutil.rmtree(configdir)
 
     cleanlist = pathObj.isce_clean_list()
-    for item in cleanlist[5]:
+    for item in cleanlist[0]:
         if os.path.isdir(os.path.join(inps.work_dir, item)):
             shutil.rmtree(os.path.join(inps.work_dir, item))
-
-    SAFE_files, download_flag = get_slc_list(ssaraopt=inps.ssaraopt, slcdir=inps.slc_dirname)
 
     i = 0
     runObj = rsmasRun()
     runObj.configure(inps, 'run_' + str(i) + "_download_data_and_dem")
-    runObj.downloadDataDEM(inps, download_flag)
+    runObj.downloadDataDEM(inps)
     runObj.finalize()
 
     return
@@ -50,14 +49,16 @@ class CreateRun:
 
     def __init__(self, inps):
 
-        self.customTemplateFile = inps.customTemplateFile
         self.work_dir = inps.work_dir
-        self.workflow = inps.workflow
+        self.workflow = inps.template['workflow']
         self.geo_master_dir = os.path.join(self.work_dir, pathObj.geomasterdir)
         self.squeesar_dir = os.path.join(self.work_dir, pathObj.squeesardir)
 
+        self.inps = Namespace(**inps.template)
+        self.inps.customTemplateFile = inps.customTemplateFile
+
         clean_list = pathObj.isce_clean_list()
-        for item in clean_list[1]:
+        for item in clean_list[0]:
             if os.path.isdir(os.path.join(inps.work_dir, item)):
                 shutil.rmtree(os.path.join(inps.work_dir, item))
                 
@@ -83,34 +84,27 @@ class CreateRun:
             print('')
             print('**************************')
         else:
-            self.coregistration = inps.coregistration
-
-        self.acquisitionDates, self.stackMasterDate, self.slaveDates, self.safe_dict, self.updateStack, \
-        self.allSLCs = checkCurrentStatus(inps)
+            self.coregistration = inps.template['coregistration']
+            
+        [self.acquisitionDates, self.stackMasterDate, self.slaveDates, self.safe_dict, self.updateStack] = checkCurrentStatus(self.inps)
 
         if self.updateStack:
             print('')
             print('Updating an existing stack ...')
             print('')
-            if inps.force:
+            if inps.template['force']:
                 print('')
                 print('Force connections using all dates...')
                 print('')
                 print(self.allSLCs)
-                self.pairs = selectNeighborPairs(inps, self.allSLCs, inps.num_connections, self.updateStack)  # based on stackSentinel.py
+                self.pairs = selectNeighborPairs(self.allSLCs, inps.template['num_connections'], self.updateStack)  # based on stackSentinel.py
             else:
                 print('')
                 print('Connections using only new dates...')
                 print('')
-                self.pairs = selectNeighborPairs(inps, self.slaveDates, inps.num_connections, self.updateStack)  # based on stackSentinel.py
+                self.pairs = selectNeighborPairs(self.slaveDates, inps.template['num_connections'], self.updateStack)  # based on stackSentinel.py
         else:
-            self.pairs = selectNeighborPairs(inps, self.acquisitionDates, inps.num_connections, self.updateStack)
-
-        if inps.ilist is not None:
-            self.pairs, self.acquisitionDates, self.slaveDates, self.allSLCs = customPairs(inps.ilist, inps.ilistonly, inps.work_dir,
-                                                                       inps.exclude_dates, self.pairs, self.acquisitionDates,
-                                                                       self.safe_dict, inps.master_date, self.slaveDates,
-                                                                       self.allSLCs)  ###SSS 7/9/2018: Add custom pairs.
+            self.pairs = selectNeighborPairs(self.acquisitionDates, inps.template['num_connections'], self.updateStack)
 
         print('*****************************************')
         print('Coregistration method: ', self.coregistration)
@@ -118,15 +112,15 @@ class CreateRun:
         print('*****************************************')
 
         self.iter = 0
-
-        self.inps = inps
+        self.pairs_sm = []
 
         return
 
     def run_stack_workflow(self):        ### This part is for isceStack run_files
+
         if self.workflow == 'interferogram':
             interferogramStack(self.inps, self.acquisitionDates, self.stackMasterDate, self.slaveDates,
-                               self.safe_dict, self.pairs, self.updateStack, self.allSLCs)
+                               self.safe_dict, self.pairs, self.updateStack)
             
         elif self.workflow == 'slc':
             slcStack(self.inps, self.acquisitionDates, self.stackMasterDate, self.slaveDates, self.safe_dict,
@@ -158,18 +152,15 @@ class CreateRun:
     def general_stack(self, inps):
 
         i = self.iter + 1
-        runObj = rsmasRun()
-        runObj.configure(inps, 'run_' + str(i) + "_hazard_products")
-        runObj.generateHazardProducts(inps)
-        runObj.finalize()
 
-        i += 1
-        runObj = rsmasRun()
-        runObj.configure(inps, 'run_' + str(i) + "_email_pysar")
-        runObj.emailPySAR(inps)
-        runObj.finalize()
+        if inps.hazard_products_flag == 'True':
+            runObj = rsmasRun()
+            runObj.configure(inps, 'run_' + str(i) + "_hazard_products")
+            runObj.generateHazardProducts(inps)
+            runObj.finalize()
 
-        i += 1
+            i += 1
+
         runObj = rsmasRun()
         runObj.configure(inps, 'run_' + str(i) + "_ingest_insarmaps")
         runObj.ingestInsarmaps(inps)
@@ -177,8 +168,8 @@ class CreateRun:
 
         i += 1
         runObj = rsmasRun()
-        runObj.configure(inps, 'run_' + str(i) + "_email_insarmaps")
-        runObj.emailInsarmaps(inps)
+        runObj.configure(inps, 'run_' + str(i) + "_email_results")
+        runObj.emailResults(inps)
         runObj.finalize()
 
         self.iter = i
@@ -201,10 +192,7 @@ class CreateRun:
             self.iter = i
             self.general_stack(inps)
 
-
         elif inps.processingMethod == 'squeesar' or inps.workflow == 'slc':
-
-            self.pairs_sm = []
 
             for i in range(len(self.acquisitionDates) - 1):
                 self.pairs_sm.append((self.acquisitionDates[0], self.acquisitionDates[i + 1]))
