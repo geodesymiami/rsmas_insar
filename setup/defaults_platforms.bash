4fdfa5f1ebadb060e7d59a05b4bef93f5ab37c55
falkamelung <famelung@rsmas.miami.edu>
2019-08-29 20:45:12 -0400

    simplified setup

--- setup/defaults_platforms.bash
+++ setup/defaults_platforms.bash
@@ -1,105 +1,95 @@
 ###############################################
-echo "sourcing $PWD/bashfiles/platforms.bash ..."
+echo "sourcing $PWD/setup/setup_miami.bash ..."
+[ -z ${USER_PREFERRED} ] && export USER_PREFERRED=$USER
+[ -z ${NOTIFICATIONEMAIL} ] && export NOTIFICATIONEMAIL=${USER_PREFERRED}\@rsmas.miami.edu
+[ -z ${RSMASINSAR_HOME} ] && export RSMASINSAR_HOME=$PWD
 
+######  required variables ##################
+# WORKDIR, SCRATCHDIR, JOBSCHEDULER, QUEUENAME
+# customizable variables: SENTINEL_ORBITS, WEATHER_DIR, TESTDATA_ISCE, DOWNLOADHOST, PROJECTNAME
+
+export WORKDIR=~/insarlab
+export SCRATCHDIR=/projects/scratch/insarlab/${USER_PREFERRED}
+export JOBSCHEDULER=LSF
+export QUEUENAME=general
+
+#############################################
+########### known platforms ##################
+#############################################
+
+########### pegasus  #########################
 if [[ (${HOST} == login3) || (${HOST} == login4) || (${HOST} =~ vis) ]]  
 then
-  export PLATFORM=pegasus
   export JOBSCHEDULER=LSF
   export QUEUENAME=general
   export PROJECTNAME =insarlab
-  export SCRATCHDIR=/projects/scratch/insarlab/${USER}
-  export WORKDIR=~/insarlab
-  export NOTIFICATIONEMAIL=${USER}\@rsmas.miami.edu
+  export SCRATCHDIR=/projects/scratch/insarlab/${USER_PREFERRED}
   export TESTDATA_ISCE=/nethome/dwg11/insarlab/TESTDATA_ISCE
   export DOWNLOADHOST=visx.ccs.miami.edu
   export SENTINEL_ORBITS=/nethome/swdowinski/S1orbits
   export SENTINEL_AUX=/nethome/swdowinski/S1aux
   export WEATHER_DIR=/nethome/dwg11/insarlab/WEATHER
-  export TESTDATADIR=visx.ccs.miami.edu:/famelung/famelung/testdata
-  export MAKEDEM_BIN=/nethome/famelung/test/testq/rsmas_insar/sources/roipac/BIN/LIN
-  export MATLABHOME=/share/opt/MATLAB/R2018b
-
-  export PATH=${PATH}:${MATLABHOME}/bin:${MAKEDEM_BIN}
 fi
 ###############################################
 if [[ ${HOST} == eos ]]
 then
-  export PLATFORM=$HOST
   export JOBSCHEDULER=PBS
   export QUEUENAME=batch
   export PROJECTNAME =insarlab
-  export SCRATCHDIR=/scratch/insarlab/${USER}
-  export WORKDIR=~/insarlab
-  export NOTIFICATIONEMAIL=${USER}\@rsmas.miami.edu
+  export SCRATCHDIR=/scratch/insarlab/${USER_PREFERRED}
 fi
 ###############################################
 if [[ ${HOST} == centos7.bogon105.com ]]
 then
-  export PLATFORM=$HOST
   export JOBSCHEDULER=PBS
   export QUEUENAME=batch
   export PROJECTNAME =insarlab
   export SCRATCHDIR=/data/rsmasdata/famelung/SCRATCHDIR
-  export WORKDIR=~/insarlab
-  export NOTIFICATIONEMAIL=${USER}\@rsmas.miami.edu
   export TESTDATA_ISCE=/data/rsmasdata/famelung/TESTDATA_ISCE
-  export SENTINEL_ORBITS=/home/famelung/insarlab/S1orbits
-  export SENTINEL_AUX=/home/famelung/insarlab/S1aux
 fi
 ###############################################
 if [[ ${HOST} == pgftsunami ]]
 then
-  export PLATFORM=$HOST
   export JOBSCHEDULER=PBS
   export QUEUENAME=batch
   export PROJECTNAME =insarlab
   export SCRATCHDIR=/data1/Projects/insar/data
-  export WORKDIR=~/insarlab
-  export NOTIFICATIONEMAIL=${USER}\@rsmas.miami.edu  
   export TESTDATA_ISCE=/data1/Projects/insar/data/TESTDATA_ISCE
 fi
 ###############################################
 if [[ ${HOST} =~ glic ]]
 then
-  export PLATFORM=$HOST
   export JOBSCHEDULER=LSF
   export QUEUENAME=serial
   export PROJECTNAME =insarlab
   export SCRATCHDIR=/data/scratch/${USER}
-  export WORKDIR=~/insarlab
-  export NOTIFICATIONEMAIL=${USER}\@rsmas.miami.edu
   export TESTDATA_ISCE=/data/Projects/insar/data/TESTDATA_ISCE
 fi
 ###############################################
 if [[ ${HOST} =~ mefe ]]
 then
-  export PLATFORM=$HOST
   export JOBSCHEDULER=PBS
   export QUEUENAME=batch
   export PROJECTNAME =insarlab
   export SCRATCHDIR=/misc/zs4
-  export WORKDIR=~/insarlab
-  export NOTIFICATIONEMAIL=${USER}\@rsmas.miami.edu
   export TESTDATA_ISCE=/misc/zs4/TESTDATA_ISCE
 fi
 
 ###############################################
 if [[ ${HOST} == *local ]]
 then
-  export SCRATCHDIR=/Users/${USER}/Documents/insarlab/scratch
   export WORKDIR=/Users/${USER}/Documents/insarlab
-  export NOTIFICATIONEMAIL=${USER}\@rsmas.miami.edu
+  export SCRATCHDIR=/Users/${USER}/Documents/insarlab/scratch
 fi
 
 ###############################################
 if [[ ${HOSTNAME} == *stampede* ]]
 then
-  export SCRATCHDIR=${SCRATCH}
   export JOBSCHEDULER=SLURM
   export QUEUENAME=normal
   export PROJECTNAME=TG-EAR180014
   export WORKDIR=${WORK}/insarlab
-  export NOTIFICATIONEMAIL=${USER}\@rsmas.miami.edu
+  export SCRATCHDIR=${SCRATCH}
 fi
 
 
---
+++ setup/environment.bash
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+echo "sourcing ${RSMASINSAR_HOME}/setup/environment.bash ..."
+#####################################
+# Setting the environment (don't modify) 
+# Required:  RSMASINSAR_HOME, WORKDIR, SCRATCHDIR
+
+# todo:
+# 
+############ FOR PROCESSING  #########
+export SSARAHOME=${RSMASINSAR_HOME}/3rdparty/SSARA
+export SSARA_ASF=${RSMASINSAR_HOME}/sources/ssara_ASF
+export ISCE_HOME=${RSMASINSAR_HOME}/3rdparty/miniconda3/lib/python3.7/site-packages/isce
+export ISCE_STACK=${RSMASINSAR_HOME}/sources/isceStack/topsStack
+export MINTPY_HOME=${RSMASINSAR_HOME}/sources/MintPy
+export MINOPY_HOME=${RSMASINSAR_HOME}/sources/minopy
+export JOBDIR=${WORKDIR}/JOBS
+export OPERATIONS=${WORKDIR}/OPERATIONS
+
+#  possibly customized variables:
+[ -z ${SENTINEL_ORBITS} ] && export SENTINEL_ORBITS=${WORKDIR}/S1orbits
+[ -z ${SENTINEL_AUX} ] && export SENTINEL_AUX=${WORKDIR}/S1aux
+[ -z ${WEATHER_DIR} ] && export WEATHER_DIR=${WORKDIR}/WEATHER
+[ -z ${TESTDATA_ISCE} ] && export TESTDATA_ISCE=${WORKDIR}/TESTDATA_ISCE
+[ -z ${DOWNLOADHOST} ] && export DOWNLOADHOST=local
+[ -z ${PROJECTNAME} ] && export PROJECTNAME=insarlab
+
+
+############ FOR MODELLING  ###########
+export GEODMOD_INFILES=${WORKDIR}/GEODMOD_INFILES
+export GEODMOD_HOME=${RSMASINSAR_HOME}/sources/geodmod
+export GEODMOD_TESTDATA=${RSMASINSAR_HOME}/data/testdata/geodmod
+export GEODMOD_TESTBENCH=${SCRATCHDIR}/GEODMOD_TESTBENCH
+
+###########  USEFUL VARIABLES  #########
+export SAMPLESDIR=${RSMASINSAR_HOME}/samples
+export DEMDIR=${WORKDIR}/DEMDIR
+export TEMPLATES=${WORKDIR}/infiles/${USER_PREFERRED}/TEMPLATES
+export TE=${TEMPLATES}
+
+############## DASK ##############
+export DASK_CONFIG=${RSMASINSAR_HOME}/minsar/defaults/dask
+export DASK_CONFIG=${RSMASINSAR_HOME}/sources/MintPy/mintpy/defaults
+
+##############  PYTHON  ##############
+export PYTHON3DIR=${RSMASINSAR_HOME}/3rdparty/miniconda3
+export CONDA_ENVS_PATH=${PYTHON3DIR}/envs
+export CONDA_PREFIX=${PYTHON3DIR}
+export PROJ_LIB=${PYTHON3DIR}/share/proj
+export GDAL_DATA=${PYTHON3DIR}/share/gdal
+
+export PYTHONPATH=${PYTHONPATH-""}
+export PYTHONPATH=${PYTHONPATH}:${MINTPY_HOME}
+export PYTHONPATH=${PYTHONPATH}:${INT_SCR}
+export PYTHONPATH=${PYTHONPATH}:${SSARA_ASF}
+export PYTHONPATH=${PYTHONPATH}:${PYTHON3DIR}/lib/python3.7/site-packages:${ISCE_HOME}:${ISCE_HOME}/components
+export PYTHONPATH=${PYTHONPATH}:${MINOPY_HOME}
+export PYTHONPATH=${PYTHONPATH}:${RSMAS_INSAR}
+export PYTHONPATH=${PYTHONPATH}:${RSMASINSAR_HOME}/sources/rsmas_tools
+export PYTHONPATH=${PYTHONPATH}:${RSMASINSAR_HOME}/3rdparty/PyAPS
+export PYTHONPATH=${PYTHONPATH}:${ISCE_STACK}
+export PYTHONPATH=${PYTHONPATH}:${RSMAS_INSAR}/sources      # needed for mimt. Need to talk to Sara on how to do this smarter
+export PYTHONPATH_RSMAS=${PYTHONPATH}
+
+######### Ignore warnings ############
+export PYTHONWARNINGS="ignore:Unverified HTTPS request"
+
+#####################################
+############  PATH  #################
+#####################################
+export PATH=${PATH}:${SSARAHOME}
+export PATH=${PATH}:${SSARA_ASF}
+export PATH=${PATH}:${MINOPY_HOME}
+export PATH=${PATH}:${RSMAS_INSAR}/minsar:${RSMAS_INSAR}/minsar/utils
+export PATH=${PATH}:${RSMASINSAR_HOME}/minsar
+export PATH=${PATH}:${RSMASINSAR_HOME}/setup/accounts
+export PATH=${PATH}:${RSMASINSAR_HOME}/sources/rsmas_tools/SAR:${RSMASINSAR_HOME}/sources/rsmas_tools/GPS:${RSMASINSAR_HOME}/sources/rsmas_tools/notebooks
+export PATH=${ISCE_HOME}/applications:${ISCE_HOME}/bin:${ISCE_STACK}:${PATH}
+export PATH=${PATH}:${RSMAS_INSAR}/sources/MimtPy
+export PATH=${PATH}:${MINTPY_HOME}/mintpy:${MINTPY_HOME}/sh
+export PATH=${PYTHON3DIR}/bin:${PATH}
+export PATH=${PATH}:${PROJ_LIB}
+export PATH=${PATH}:${RSMASINSAR_HOME}/3rdparty/tippecanoe/bin
+export PATH=${PATH}:${DASK_CONFIG}
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH-""}
+export LD_LIBRARY_PATH=${PYTHON3DIR}/lib
+export LD_RUN_PATH=${PYTHON3DIR}/lib
+
+if [ -n "${prompt}" ]
+then
+    echo "RSMASINSAR_HOME:" ${RSMASINSAR_HOME}
+    echo "PYTHON3DIR:     " ${PYTHON3DIR}
+    echo "SSARAHOME:      " ${SSARAHOME}
+fi
