774ac7004db7a53c938de7e52f220778e76ab10e
Sara Mirzaee <sxm1611@login4.pegasus.edu>
2019-09-03 20:11:42 -0400

    correct walltime format based on jobscheduler

--- minsar/utils/process_utilities.py
+++ minsar/utils/process_utilities.py
@@ -550,7 +550,7 @@ def walltime_adjust(inps, default_time):
     hour = (default_time_hour * number_of_bursts)*60
     minutes = int(np.remainder(hour, 60))
     hour = int(hour/60)
-    adjusted_time = '{:02d}:{:02d}:{:02d}'.format(hour, minutes, 0)
+    adjusted_time = '{:02d}:{:02d}'.format(hour, minutes)
 
     return adjusted_time
 
@@ -569,7 +569,7 @@ def add_pause_to_walltime(wall_time, wait_time):
 
     wait_seconds = (wait_parts[0] * 60 + wait_parts[1]) * 60
 
-    new_wall_time = '{:02d}:{:02d}:{:02d}'.format(hours, minutes, 0)
+    new_wall_time = '{:02d}:{:02d}'.format(hours, minutes)
 
     return wait_seconds, new_wall_time
 
