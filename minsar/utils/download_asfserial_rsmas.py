24bfb34560566370b1ad567b5b7329d7ecf3cb05
Alexandros Ali Khan <axk1168@login3.pegasus.edu>
2020-01-21 22:31:37 -0500

    parallel_download 2

--- minsar/utils/download_asfserial_rsmas.py
+++ minsar/utils/download_asfserial_rsmas.py
@@ -63,67 +63,52 @@ def main(iargs=None):
     dataset_template = Template(inps.custom_template_file)
     dataset_template.options.update(PathFind.correct_for_ssara_date_format(dataset_template.options))
     subprocess.Popen("rm new_files.csv", shell=True).wait()
-    standard_tuple = (inps, dataset_template)
     seasonal_start_date = None
     seasonal_end_date = None
+
     try:
         if dataset_template.options['seasonalStartDate'] is not None and dataset_template.options['seasonalEndDate'] is not None:
             seasonal_start_date = dataset_template.options['seasonalStartDate']
             seasonal_end_date = dataset_template.options['seasonalEndDate']
     except:
         pass
+
     if inps.seasonalStartDate is not None and inps.seasonalEndDate is not None:
         seasonal_start_date = inps.seasonalStartDate
         seasonal_end_date = inps.seasonalEndDate
+
     if seasonal_start_date is not None and seasonal_end_date is not None:
-        original_start_year = int(dataset_template.options['ssaraopt.startDate'][:4])
-        if int(seasonal_start_date) > int(seasonal_end_date):
-            offset = 1
-        else:
-            offset = 0
-        year_range = int(dataset_template.options['ssaraopt.endDate'][:4]) - original_start_year + 1
-        if year_range > 1 and offset == 1:
-            year_range = year_range - 1
-        ssaraopt_seasonal_start_date = '-' + seasonal_start_date[:2] + '-' + seasonal_start_date[2:]
-        seasonalEndDateAddOn = '-' + seasonal_end_date[:2] + '-' + seasonal_end_date[2:]
-        original_end_date = dataset_template.options['ssaraopt.endDate']
-        for counter in range(year_range):
-            seasonal_tuple = standard_tuple + (counter, original_start_year, offset, year_range, ssaraopt_seasonal_start_date, ssaraopt_seasonal_end_date, original_end_date)
-            generate_files_csv(project_slc_dir, inps.custom_template_file, seasonal_tuple)
-            counter += 1
+        generate_seasonal_files_csv(dataset_template, seasonal_start_date, seasonal_end_date)
     else:
-        generate_files_csv(project_slc_dir, inps.custom_template_file, standard_tuple)
+        generate_files_csv(project_slc_dir, dataset_template)
+
     parallel = False
+
     try:
-        if dataset_template.options['parallel'] != 'yes':
+        if dataset_template.options['parallel'] == 'yes':
             parallel = True
     except:
         pass
-    if inps.parallel != 'yes':
-        parallel = True
+
+    """if inps.parallel == 'yes':
+        parallel = True"""
+
     processes = os.cpu_count()
+
     try:
         if dataset_template.options['processes'] is not None:
-            processes = dataset_template.options['processes']
+            processes = int(dataset_template.options['processes'])
     except:
         pass
-    if inps.processes is not None:
-        processes = inps.processes
+
+    """if inps.processes is not None:
+        processes = inps.processes"""
+
     if parallel:
-        comma = '^[^,]+,?'
-        num = 1
-        csv_chunk_files = []
-        while os.stat(project_slc_dir + '/new_files.csv').st_size == 0:
-            subprocess.Popen("grep -E -o '" + comma + "' " + project_slc_dir + "/new_files.csv > " + project_slc_dir + "/new_files" + num + ".csv", shell=True).wait()
-            subprocess.Popen("sed -r -i 's/" + comma + "//' " + project_slc_dir + "/new_files.csv", shell=True).wait()
-            num += 1
-            if num > processes:
-                num = 1
-        for num in range(1, processes + 1):
-            subprocess.Popen("sed -r -i 's/,$//' " + project_slc_dir + "/new_files" + num + ".csv", shell=True).wait()
-            csv_chunk_files.append('new_files' + num + '.csv')
-        Pool(processes).map(run_parallel_download_asf_serial_helper, csv_chunk_files)
-    succesful = run_download_asf_serial(project_slc_dir, logger)
+        succesful = run_parallel_download_asf_serial(project_slc_dir, processes)
+    else:
+        succesful = run_download_asf_serial(project_slc_dir, logger)
+
     change_file_permissions()
     logger.log(loglevel.INFO, "SUCCESS: %s", str(succesful))
     logger.log(loglevel.INFO, "------------------------------------")
@@ -131,62 +116,98 @@ def main(iargs=None):
     return None
 
 
-def generate_files_csv(slc_dir, custom_template_file, tupleParam):
+def generate_seasonal_files_csv(dataset_template, seasonal_start_date, seasonal_end_date):
+    """ 
+    """
+    original_start_year = int(dataset_template.options['ssaraopt.startDate'][:4])
+    if int(seasonal_start_date) > int(seasonal_end_date):
+        offset = 1
+    else:
+        offset = 0
+    year_range = int(dataset_template.options['ssaraopt.endDate'][:4]) - original_start_year + 1
+    if year_range > 1 and offset == 1:
+        year_range = year_range - 1
+    ssaraopt_seasonal_start_date = '-' + seasonal_start_date[:2] + '-' + seasonal_start_date[2:]
+    ssaraopt_seasonal_end_date = '-' + seasonal_end_date[:2] + '-' + seasonal_end_date[2:]
+    original_end_date = dataset_template.options['ssaraopt.endDate']
+    for counter in range(year_range):
+        seasonal_tuple = (counter, original_start_year, offset, year_range, ssaraopt_seasonal_start_date, ssaraopt_seasonal_end_date, original_end_date)
+        dates = generate_seasonal_ssaraopt_dates(dataset_template, seasonal_start_date, seasonal_end_date, seasonal_tuple)
+        if dates is not None:
+            generate_files_csv(project_slc_dir, dataset_template, dates[0], dates[1])
+        counter += 1
+
+
+def generate_seasonal_ssaraopt_dates(dataset_template, seasonal_start_date, seasonal_end_date, tuple_param):
+    """ 
+    """
+    counter = tuple_param[0]
+    original_start_year = tuple_param[1]
+    offset = tuple_param[2]
+    year_range = tuple_param[3]
+    ssaraopt_seasonal_start_date = tuple_param[4]
+    ssaraopt_seasonal_end_date = tuple_param[5]
+    original_end_date = tuple_param[6]
+
+    start_date = dataset_template.options['ssaraopt.startDate']
+    end_date = dataset_template.options['ssaraopt.endDate']
+
+    if counter == 0:
+        if year_range == 1:
+            if offset == 0:
+                if int(seasonal_end_date) < int(dataset_template.options['ssaraopt.startDate'][4:].replace('-', '')) or int(seasonal_start_date) > int(original_end_date[4:].replace('-', '')):
+                    return None
+                if int(seasonal_start_date) >  int(dataset_template.options['ssaraopt.startDate'][4:].replace('-', '')):
+                    start_date = str(original_start_year) + ssaraopt_seasonal_start_date
+                if int(seasonal_end_date) < int(original_end_date[4:].replace('-', '')):
+                    end_date = str(original_start_year) + ssaraopt_seasonal_end_date
+            elif int(dataset_template.options['ssaraopt.endDate'][:4]) - original_start_year + 1 == 1:
+                if int(seasonal_start_date) > int(original_end_date[4:].replace('-', '')):
+                    return None
+                if int(seasonal_start_date) >  int(dataset_template.options['ssaraopt.startDate'][4:].replace('-', '')):
+                    start_date = str(original_start_year) + ssaraopt_seasonal_start_date
+            else:
+                if int(seasonal_start_date) >  int(dataset_template.options['ssaraopt.startDate'][4:].replace('-', '')):
+                    start_date = str(original_start_year) + ssaraopt_seasonal_start_date
+                if int(seasonal_end_date) < int(original_end_date[4:].replace('-', '')):
+                    end_date = str(original_start_year + offset) + ssaraopt_seasonal_end_date
+        else:
+            if offset == 0:
+                if int(seasonal_end_date) < int(dataset_template.options['ssaraopt.startDate'][4:].replace('-', '')):
+                    return None
+                if int(seasonal_start_date) >  int(dataset_template.options['ssaraopt.startDate'][4:].replace('-', '')):
+                    start_date = str(original_start_year) + ssaraopt_seasonal_start_date
+                end_date = str(original_start_year) + ssaraopt_seasonal_end_date
+            else:
+                if int(seasonal_start_date) >  int(dataset_template.options['ssaraopt.startDate'][4:].replace('-', '')):
+                    start_date = str(original_start_year) + ssaraopt_seasonal_start_date
+                end_date = str(original_start_year + offset) + ssaraopt_seasonal_end_date
+    elif counter < year_range - 1:
+        start_date = str(original_start_year + counter) + ssaraopt_seasonal_start_date
+        end_date = str(original_start_year + offset) + ssaraopt_seasonal_end_date
+    elif counter == year_range - 1:
+        if int(seasonal_end_date) < int(original_end_date[4:].replace('-', '')):
+            end_date = str(original_start_year + offset) + ssaraopt_seasonal_end_date
+        else:
+            end_date = original_end_date
+        start_date = str(original_start_year + counter) + ssaraopt_seasonal_start_date
+
+    return (start_date, end_date)
+
+
+def generate_files_csv(slc_dir, custom_template_file, start_date=None, end_date=None):
     """ Generates a csv file of the files to download serially.
     Uses the `awk` command to generate a csv file containing the data files to be download
     serially. The output csv file is then sent through the `sed` command to remove the first five
     empty values to eliminate errors in download_ASF_serial.py.
     """
-    
-    inps = tupleParam[0]
-    dataset_template = tupleParam[1]
-    if seasonal_start_date is not None and seasonal_end_date is not None:
-        counter = tupleParam[2]
-        original_start_year = tupleParam[3]
-        offset = tupleParam[4]
-        year_range = tupleParam[5]
-        ssaraopt_seasonal_start_date = tupleParam[6]
-        ssaraopt_seasonal_end_date = tupleParam[7]
-        original_end_date = tupleParam[8]
-        if counter == 0:
-            if year_range == 1:
-                if offset == 0:
-                    if int(seasonal_end_date) < int(dataset_template.options['ssaraopt.startDate'][4:].replace('-', '')) or int(seasonal_start_date) > int(original_end_date[4:].replace('-', '')):
-                        return
-                    if int(seasonal_start_date) >  int(dataset_template.options['ssaraopt.startDate'][4:].replace('-', '')):
-                        dataset_template.options['ssaraopt.startDate'] = str(original_start_year) + ssaraopt_seasonal_start_date
-                    if int(seasonal_end_date) < int(original_end_date[4:].replace('-', '')):
-                        dataset_template.options['ssaraopt.endDate'] = str(original_start_year) + ssaraopt_seasonal_end_date
-                elif int(dataset_template.options['ssaraopt.endDate'][:4]) - original_start_year + 1 == 1:
-                    if int(seasonal_start_date) > int(original_end_date[4:].replace('-', '')):
-                        return
-                    if int(seasonal_start_date) >  int(dataset_template.options['ssaraopt.startDate'][4:].replace('-', '')):
-                        dataset_template.options['ssaraopt.startDate'] = str(original_start_year) + ssaraopt_seasonal_start_date
-                else:
-                    if int(seasonal_start_date) >  int(dataset_template.options['ssaraopt.startDate'][4:].replace('-', '')):
-                        dataset_template.options['ssaraopt.startDate'] = str(original_start_year) + ssaraopt_seasonal_start_date
-                    if int(seasonal_end_date) < int(original_end_date[4:].replace('-', '')):
-                        dataset_template.options['ssaraopt.endDate'] = str(original_start_year + offset) + ssaraopt_seasonal_end_date
-            else:
-                if offset == 0:
-                    if int(seasonal_end_date) < int(dataset_template.options['ssaraopt.startDate'][4:].replace('-', '')):
-                        return 
-                    if int(seasonal_start_date) >  int(dataset_template.options['ssaraopt.startDate'][4:].replace('-', '')):
-                        dataset_template.options['ssaraopt.startDate'] = str(original_start_year) + ssaraopt_seasonal_start_date
-                    dataset_template.options['ssaraopt.endDate'] = str(original_start_year) + ssaraopt_seasonal_end_date
-                else:
-                    if int(seasonal_start_date) >  int(dataset_template.options['ssaraopt.startDate'][4:].replace('-', '')):
-                        dataset_template.options['ssaraopt.startDate'] = str(original_start_year) + ssaraopt_seasonal_start_date
-                    dataset_template.options['ssaraopt.endDate'] = str(original_start_year + offset) + ssaraopt_seasonal_end_date
-        elif counter < year_range - 1:
-            dataset_template.options['ssaraopt.startDate'] = str(original_start_year + counter) + ssaraopt_seasonal_start_date
-            dataset_template.options['ssaraopt.endDate'] = str(original_start_year + offset) + ssaraopt_seasonal_end_date
-        elif counter == year_range - 1:
-            if int(seasonal_end_date) < int(original_end_date[4:].replace('-', '')):
-                dataset_template.options['ssaraopt.endDate'] = str(original_start_year + offset) + ssaraopt_seasonal_end_date
-            else:
-                dataset_template.options['ssaraopt.endDate'] = original_end_date
-            dataset_template.options['ssaraopt.startDate'] = str(original_start_year + counter) + ssaraopt_seasonal_start_date
+
+    dataset_template = custom_template_file
+
+    if start_date is not None and end_date is not None:
+        dataset_template.options['ssaraopt.startDate'] = start_date
+        dataset_template.options['ssaraopt.endDate'] = end_date
+
     ssaraopt = dataset_template.generate_ssaraopt_string()
     ssaraopt = ssaraopt.split(' ')
 
@@ -205,6 +226,30 @@ def generate_files_csv(slc_dir, custom_template_file, tupleParam):
                   ">>" + os.path.join(slc_dir, 'new_files.csv')
     message_rsmas.log(slc_dir, sed_command)
     subprocess.Popen(sed_command, shell=True).wait()
+
+
+def run_parallel_download_asf_serial(project_slc_dir, processes):
+    """ 
+    """
+    comma = '^[^,]+,?'
+    file_num = 1
+    total_num = 0
+    csv_chunk_files = []
+
+    while os.stat(project_slc_dir + '/new_files.csv').st_size != 0:
+        subprocess.Popen("grep -E -o '" + comma + "' " + project_slc_dir + "/new_files.csv > " + project_slc_dir + "/new_files" + str(file_num) + ".csv", shell=True).wait()
+        subprocess.Popen("sed -r -i 's/" + comma + "//' " + project_slc_dir + "/new_files.csv", shell=True).wait()
+        file_num += 1
+        total_num += 1
+        if file_num > processes:
+            file_num = 1
+    if total_num < processes:
+        processes = total_num
+    for file_num in range(1, processes + 1):
+        subprocess.Popen("sed -r -i 's/,$//' " + project_slc_dir + "/new_files" + str(file_num) + ".csv", shell=True).wait()
+        csv_chunk_files.append('new_files' + str(file_num) + '.csv')
+
+    return Pool(processes).map(run_parallel_download_asf_serial_helper, csv_chunk_files)
 
 def run_parallel_download_asf_serial_helper(csv_chunk_file):
     run_download_asf_serial(project_slc_dir, logger, csv_file=csv_chunk_file)
