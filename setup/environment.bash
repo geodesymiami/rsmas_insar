14c63d7257bee6a09e47b551721eed8de03e901c
falkamelung <famelung@rsmas.miami.edu>
2020-06-11 10:21:31 -0500

    environment.bash: removed isceStack from ISCE_STACK path

--- setup/environment.bash
+++ setup/environment.bash
@@ -22,7 +22,8 @@ echo "sourcing ${RSMASINSAR_HOME}/setup/environment.bash ..."
 ############ FOR PROCESSING  #########
 export SSARAHOME=${RSMASINSAR_HOME}/3rdparty/SSARA
 export ISCE_HOME=${RSMASINSAR_HOME}/3rdparty/miniconda3/lib/python3.7/site-packages/isce
-export ISCE_STACK=${RSMASINSAR_HOME}/sources/isceStack/isce2/contrib/stack/topsStack
+#export ISCE_STACK=${RSMASINSAR_HOME}/sources/isce2/contrib/stack/stripmapStack
+export ISCE_STACK=${RSMASINSAR_HOME}/sources/isce2/contrib/stack/topsStack
 export MINTPY_HOME=${RSMASINSAR_HOME}/sources/MintPy
 export MINOPY_HOME=${RSMASINSAR_HOME}/sources/minopy
 export MIMTPY_HOME=${RSMASINSAR_HOME}/sources/MimtPy
