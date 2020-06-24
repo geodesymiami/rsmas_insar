b30b098a895021b6d136f84eaa950697f1bb59fb
Bhuvan Varugu <tg864867@login3.stampede2.tacc.utexas.edu>
2020-06-24 16:58:45 -0500

    adding stripmap support in smallbaseline wrapper of minsar

--- minsar/smallbaseline_wrapper.py
+++ minsar/smallbaseline_wrapper.py
@@ -6,12 +6,14 @@
 import os
 import sys
 import time
+import subprocess
 from minsar.objects import message_rsmas
 from minsar.objects.auto_defaults import PathFind
 import minsar.utils.process_utilities as putils
 from minsar.job_submission import JOB_SUBMIT
 from minsar import email_results
 from mintpy import smallbaselineApp
+from mintpy import prep_isce
 import contextlib
 
 pathObj = PathFind()
@@ -31,7 +33,25 @@ def main(iargs=None):
     message_rsmas.log(inps.work_dir, os.path.basename(__file__) + ' ' + ' '.join(input_arguments))
 
     time.sleep(putils.pause_seconds(inps.wait_time))
-
+    
+    #########################################
+    # stripmap prep to isce
+    #########################################
+    if inps.template['acquisition_mode']=='stripmap':
+        inps.dsetDir = inps.work_dir +'/Igrams';
+        inps.slcDir  = inps.work_dir +'/merged/SLC';
+        inps.geometryDir = inps.work_dir +'/geom_master';
+        inps.baselineDir = inps.work_dir +'/baselines';
+        masterDate= inps.template['stripmapStack.master']
+        if masterDate=='None':
+            command1= 'cp -r '+inps.slcDir+'/'+os.listdir(inps.slcDir)[0]+'/'+'masterShelve '+inps.work_dir+'/.';
+        else:
+            command1= 'cp -r '+inps.slcDir+'/' + masterDate+'/'+'masterShelve '+inps.work_dir+'/.';
+        print(command1);subprocess.Popen(command1, shell=True).wait();
+        inps.metaFile= inps.work_dir+'/' +'masterShelve/data.dat';
+        command2= 'prep_isce.py -d '+inps.dsetDir+' -m '+inps.metaFile+' -b '+inps.baselineDir+' -g '+inps.geometryDir; 
+        print(command2)
+        subprocess.Popen(command2, shell=True).wait();
     #########################################
     # Submit job
     #########################################
