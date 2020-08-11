81d326fe5de723f1fb0c7d128a9a4bf26f889778
Sara Mirzaee <sara.mirzaee@rsmas.miami.edu>
2020-08-11 13:54:29 -0500

    add known_issues.md to print if there is an error

--- minsar/check_job_outputs.py
+++ minsar/check_job_outputs.py
@@ -23,8 +23,12 @@ def cmd_line_parser(iargs=None):
 def main(iargs=None):
 
     inps = cmd_line_parser(iargs)
-    work_dir = os.path.dirname(inps.batch_job)
+    work_dir = os.path.dirname(os.path.abspath(inps.batch_job))
+    project_dir = os.path.dirname(work_dir)
     job_name = inps.batch_job.split('.')[0]
+    knwon_issues_file = os.path.join(os.getenv('RSMASINSAR_HOME'), 'minsar/docs/known_issues.md')
+
+    error_happened = False
 
     error_files = glob.glob(job_name + '*.e')
     for errfile in error_files:
@@ -33,17 +37,24 @@ def main(iargs=None):
                     check_words_in_file(errfile, 'ERROR'),
                     check_words_in_file(errfile, 'Error')]
         if np.array(job_exit).any():
-            raise RuntimeError('Error terminating job: {}'.format(inps.batch_job))
+            error_happened = True
+            shutil.copy2(knwon_issues_file, project_dir)
+            with open(knwon_issues_file, 'r') as f:
+                known_issues = f.read()
+            print(known_issues)
 
     putils.remove_zero_size_or_length_error_files(run_file=job_name)
     putils.raise_exception_if_job_exited(run_file=job_name)
-    putils.concatenate_error_files(run_file=job_name, work_dir=os.path.dirname(work_dir))
+    putils.concatenate_error_files(run_file=job_name, work_dir=project_dir)
 
     out_folder = work_dir + '/stdout_' + os.path.basename(inps.batch_job)
     if os.path.exists(out_folder):
         shutil.rmtree(out_folder)
 
     putils.move_out_job_files_to_stdout(run_file=job_name)
+
+    if error_happened:
+        raise RuntimeError('Error terminating job: {}'.format(inps.batch_job))
 
     return
 
