48ea703b342e1bb9672c90a92b6af40f2bb2b673
Joshua Zahner <joshuazahner@gmail.com>
2021-01-26 14:31:46 -0600

    Updates

--- minsar/submit_jobs.bash
+++ minsar/submit_jobs.bash
@@ -104,62 +104,66 @@ function submit_job_conditional {
 
     # Compute maximum allowable tasks for this step
     step_max_tasks=$(echo "$step_max_tasks_unit/${step_io_load_list[$step_name]}" | bc | awk '{print int($1)}')
-
-    # Compute number of total active tasks and number of active tasks for curent step
-    num_active_tasks_total=$(compute_num_tasks)
-    num_active_tasks_step=$(compute_num_tasks $step_name)
-
-    # Get number of tasks associated with current jobfile
-    # insarmaps.job and smallbaseline_wrapper.job always have 1 task.
-    # ISCE runfiles have number of tasks equal to number of lines in associated launcher script
-    # Launcher script: "run_01_unpack_topo_reference_0.job" -> "run_01_unpack_topo_reference_0"    
-    num_tasks_job=$(num_tasks_for_file $file)
     
-    # Compute new total number of tasks and tasks for current step
-    new_tasks_step=$(($num_active_tasks_step+$num_tasks_job))
-    new_tasks_total=$(($num_active_tasks+$num_tasks_job))
-
-    # Get active number of running jobs
-    num_active_jobs=$(squeue -u $USER -h -t running,pending -r | wc -l )
+    time_elapsed=0
     
-    echo "Number of running/pending jobs: $num_active_jobs" >&2 
-    echo "$num_active_tasks_total running/pending tasks across all jobs (maximum $total_max_tasks)" >&2
-    echo "step $step_name: $num_active_tasks_step running/pending tasks (maximum $step_max_tasks)" >&2
-    echo "$(basename $file): $num_tasks_job additional tasks" >&2
+    while [[ $time_elapsed -lt 604800 ]]; do
+	# Compute number of total active tasks and number of active tasks for curent step
+	num_active_tasks_total=$(compute_num_tasks)
+	num_active_tasks_step=$(compute_num_tasks $step_name)
 
-    if [[ $num_active_jobs -lt $MAX_JOBS_PER_QUEUE ]] && [[ $new_tasks_step -lt $step_max_tasks ]] && [[ $new_tasks_total -lt $total_max_tasks ]]; then
-        job_submit_message=$(sbatch $file | grep "Submitted batch job")
-        exit_status="$?"
-        if [[ $exit_status -ne 0 ]]; then
-            echo "sbatch message: $job_submit_message" >&2
-            echo "sbatch submit error: exit code $exit_status. Sleep 60 seconds and try again" >&2
-            sleep 60
+	# Get number of tasks associated with current jobfile
+	# insarmaps.job and smallbaseline_wrapper.job always have 1 task.
+	# ISCE runfiles have number of tasks equal to number of lines in associated launcher script
+	# Launcher script: "run_01_unpack_topo_reference_0.job" -> "run_01_unpack_topo_reference_0"    
+	num_tasks_job=$(num_tasks_for_file $file)
+	
+	# Compute new total number of tasks and tasks for current step
+	new_tasks_step=$(($num_active_tasks_step+$num_tasks_job))
+	new_tasks_total=$(($num_active_tasks+$num_tasks_job))
+	
+	# Get active number of running jobs
+	num_active_jobs=$(squeue -u $USER -h -t running,pending -r | wc -l )
+	
+	echo "Number of running/pending jobs: $num_active_jobs" >&2 
+	echo "$num_active_tasks_total running/pending tasks across all jobs (maximum $total_max_tasks)" >&2
+	echo "step $step_name: $num_active_tasks_step running/pending tasks (maximum $step_max_tasks)" >&2
+	echo "$(basename $file): $num_tasks_job additional tasks" >&2
+	
+	if [[ $num_active_jobs -lt $MAX_JOBS_PER_QUEUE ]] && [[ $new_tasks_step -lt $step_max_tasks ]] && [[ $new_tasks_total -lt $total_max_tasks ]]; then
             job_submit_message=$(sbatch $file | grep "Submitted batch job")
             exit_status="$?"
             if [[ $exit_status -ne 0 ]]; then
-                echo "sbatch message: $job_submit_message" >&2
-                echo "sbatch submit error: exit code $exit_status. Exiting with status code 1." >&2
-                exit 1
+		echo "sbatch message: $job_submit_message" >&2
+		echo "sbatch submit error: exit code $exit_status. Sleep 60 seconds and try again" >&2
+		sleep 30
+		job_submit_message=$(sbatch $file | grep "Submitted batch job")
+		exit_status="$?"
+		if [[ $exit_status -ne 0 ]]; then
+                    echo "sbatch message: $job_submit_message" >&2
+                    echo "sbatch submit error: exit code $exit_status. Exiting with status code 1." >&2
+		    sleep 60
+		fi
             fi
-        fi
 
-        jobnumber=$(grep -oE "[0-9]{7}" <<< $job_submit_message)
+            jobnumber=$(grep -oE "[0-9]{7}" <<< $job_submit_message)
 
-        echo $jobnumber
-        return 0
-    else
-        if [[ $num_active_jobs -ge $MAX_JOBS_PER_QUEUE ]]; then
-            echo "Couldnt submit job (${file}), because there are $num_active_jobs active jobs right now (max: $MAX_JOBS_PER_QUEUE). Waiting 5 minutes to try again." >&2
-        elif [[ $new_tasks_step -ge $step_max_tasks ]]; then
-            echo "Couldnt submit job (${file}), because there would be $new_tasks_step active tasks for this step right now (max: $step_max_tasks). Waiting 5 minutes to try again." >&2
-        elif [[ $new_tasks_total -ge $total_max_tasks ]]; then
-            echo "Couldnt submit job (${file}), because there would be $new_tasks_total total active tasks right now (max: $total_max_tasks). Waiting 5 minutes to try again." >&2
-        fi
-
-        return 1
-
-    fi
-
+            echo $jobnumber
+            return 0
+	else
+            if [[ $num_active_jobs -ge $MAX_JOBS_PER_QUEUE ]]; then
+		echo "Couldnt submit job (${file}), because there are $num_active_jobs active jobs right now (max: $MAX_JOBS_PER_QUEUE). Waiting 5 minutes to try again." >&2
+            elif [[ $new_tasks_step -ge $step_max_tasks ]]; then
+		echo "Couldnt submit job (${file}), because there would be $new_tasks_step active tasks for this step right now (max: $step_max_tasks). Waiting 5 minutes to try again." >&2
+            elif [[ $new_tasks_total -ge $total_max_tasks ]]; then
+		echo "Couldnt submit job (${file}), because there would be $new_tasks_total total active tasks right now (max: $total_max_tasks). Waiting 5 minutes to try again." >&2
+            fi
+	    
+            sleep 30
+	    time_elapsed=$((time_elapsed+300))
+	    echo "Time Elapsed: $time_elapsed of 604800s (7 days)" >&2
+	fi
+    done
 }
 
 if [[ "$1" == "--help" || "$1" == "-h" ]]; then
