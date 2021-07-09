4f7fbd212b8f63b7467e34688f10e844846d597d
Sara Mirzaee <sara.mirzaee@rsmas.miami.edu>
2021-07-09 16:54:07 -0500

    copy minopy required folders to tmp

--- minsar/job_submission.py
+++ minsar/job_submission.py
@@ -679,13 +679,13 @@ class JOB_SUBMIT:
         job_file_lines.append( "cp -r $RSMASINSAR_HOME/setup  /tmp/rsmas_insar\n" )
         job_file_lines.append( "mkdir -p /tmp/rsmas_insar/3rdparty ;\n" )
 
-        if "smallbaseline_wrapper" in job_file_name or "insarmaps" in job_file_name:
+        if "smallbaseline_wrapper" in job_file_name or "insarmaps" in job_file_name or 'mintpy' in job_file_name:
             job_file_lines.append( "mkdir -p /tmp/rsmas_insar/sources\n" )
             job_file_lines.append( "cp -r $RSMASINSAR_HOME/sources/MintPy /tmp/rsmas_insar/sources\n" )
             job_file_lines.append( "cp -r $RSMASINSAR_HOME/3rdparty/PyAPS /tmp/rsmas_insar/3rdparty\n" )
             job_file_lines.append( "cp -r $RSMASINSAR_HOME/sources/insarmaps_scripts /tmp/rsmas_insar/sources\n" )
 
-        if 'minopy' in job_file_name or 'mintpy' in job_file_name:
+        if 'minopy' in job_file_name:
             job_file_lines.append("mkdir -p /tmp/rsmas_insar/sources\n")
             job_file_lines.append("cp -r $RSMASINSAR_HOME/sources/MiNoPy /tmp/rsmas_insar/sources\n")
             job_file_lines.append("cp -r $RSMASINSAR_HOME/sources/MintPy /tmp/rsmas_insar/sources\n")
