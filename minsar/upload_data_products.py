25942ee40a21670db03bd3d57590227db74dfa3f
falkamelung <famelung@rsmas.miami.edu>
2021-08-07 20:04:33 -0500

    minsarApp.bash, upload_data_products.py: minpoy plotting using mintpy colors

--- minsar/upload_data_products.py
+++ minsar/upload_data_products.py
@@ -97,6 +97,20 @@ def main(iargs=None):
             status = subprocess.Popen(command, shell=True).wait()
             if status is not 0:
                  raise Exception('ERROR in upload_data_products.py')
+        if os.path.exists(inps.work_dir + '/minopy_sequential'):
+            command = 'ssh ' + DATA_SERVER + ' mkdir -p ' + REMOTE_DIR + project_name + '/minopy_sequential'
+            print (command)
+            status = subprocess.Popen(command, shell=True).wait()
+            if status is not 0:
+                 raise Exception('ERROR in upload_data_products.py')
+        if os.path.exists(inps.work_dir + '/minopy_single_reference'):
+            command = 'ssh ' + DATA_SERVER + ' mkdir -p ' + REMOTE_DIR + project_name + '/minopy_single_reference'
+            print (command)
+            status = subprocess.Popen(command, shell=True).wait()
+            if status is not 0:
+                 raise Exception('ERROR in upload_data_products.py')
+
+
 
         for pattern in scp_list:
             if ( len(glob.glob(inps.work_dir + '/' + pattern)) >= 1 ):
