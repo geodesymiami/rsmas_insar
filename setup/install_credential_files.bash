a085600fd62849239fa9569cc69cf83f8df699b3
falkamelung <famelung@rsmas.miami.edu>
2023-01-19 12:00:27 -0600

    install_credential_files.bash: updated to new structure

--- setup/install_credential_files.bash
+++ setup/install_credential_files.bash
@@ -2,13 +2,13 @@
 ######### copy credentials to right place ##############
 
 # for ssara 
-characterCount=`wc -m ../3rdparty/SSARA/password_config.py`
+characterCount=`wc -m ../tools/SSARA/password_config.py`
 characterCount=$(echo "${characterCount[0]%% *}")
 
 if [[  $characterCount == 75 ]]; then
       echo "Use default password_config.py for SSARA (because existing file lacks passwords)"
-      echo "Copying password_config.py into ../3rdparty/SSARA"
-      cp ~/accounts/password_config.py ../3rdparty/SSARA
+      echo "Copying password_config.py into ../tools/SSARA"
+      cp ~/accounts/password_config.py ../tools/SSARA
    else
       echo File password_config.py not empty - kept unchanged
 fi
@@ -23,17 +23,17 @@ if [[ ! -f ~/.netrc ]]; then
 fi
 
 ## for pyaps 
-#if (! -f 3rdparty/PyAPS/pyaps3/model.cfg) then
-#      echo Copying default model.cfg for ECMWF download with PyAPS into ../3rdparty/PyAPS/pyaps3
-#      cp ~/accounts/model.cfg ../3rdparty/PyAPS/pyaps3
+#if (! -f tools/PyAPS/pyaps3/model.cfg) then
+#      echo Copying default model.cfg for ECMWF download with PyAPS into ../tools/PyAPS/pyaps3
+#      cp ~/accounts/model.cfg ../tools/PyAPS/pyaps3
 #   else
 #      echo File model.cfg exists already - kept unchanged
 #endif
 
 # for pyaps 
 
-python_version=$(echo "python3.$(python --version | cut -d. -f2)")
-model_cfg_file=$(echo "../3rdparty/miniconda3/lib/$python_version/site-packages/pyaps3/model.cfg")
+python_version=$(echo "python3.$(../tools/miniconda3/bin/python --version | cut -d. -f2)")
+model_cfg_file=$(echo "../tools/miniconda3/lib/$python_version/site-packages/pyaps3/model.cfg")
 if [[ ! -f $model_cfg_file ]]; then
       echo "Copying default model.cfg for ECMWF download with PyAPS into $(dirname $model_cfg_file)"
       cp ~/accounts/model.cfg $model_cfg_file
