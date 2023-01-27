314fc73d32010e98cb2c1a5ffc0af34c91df0c8a
falkamelung <famelung@rsmas.miami.edu>
2023-01-27 10:16:40 -0600

    process_utilities.py: switch to --no-tmp

--- minsar/utils/process_utilities.py
+++ minsar/utils/process_utilities.py
@@ -81,7 +81,7 @@ def add_create_runfiles(parser):
     run_parser = parser.add_argument_group('create run files and jobs options:')
     run_parser.add_argument('--tmp', dest='copy_to_tmp', action='store_true', default=True,
                             help='writes job files to run on /tmp rather than /scratch [default].')
-    run_parser.add_argument('--no_tmp', dest='copy_to_tmp', action='store_false',
+    run_parser.add_argument('--no-tmp', dest='copy_to_tmp', action='store_false',
                             help="writes job files to run on /scratch rather than /tmp.")
     run_parser.add_argument('--jobfiles', dest='write_jobs', action='store_true',
                              help='writes the jobs corresponding to run files')
