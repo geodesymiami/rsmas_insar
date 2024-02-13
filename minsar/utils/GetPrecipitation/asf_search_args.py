d9ddbfa156d4508bdbe24a2f28268cdc69b2ca5e
disilvestro <ds96@live.it>
2024-02-13 16:30:01 -0500

    asf_search_args.py: added metavar

--- minsar/utils/GetPrecipitation/asf_search_args.py
+++ minsar/utils/GetPrecipitation/asf_search_args.py
@@ -2,9 +2,12 @@
 
 import asf_search as asf
 import datetime
+import argparse# Import necessary libraries
+import datetime
 import argparse
 import os
 
+# Define the description and epilog of the script
 EXAMPLE = """example:
   
 asf_search_args.py --polygon "POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))"
@@ -18,17 +21,14 @@ workDir = 'SCRATCHDIR'
 parser = argparse.ArgumentParser()
 
 # Define your optional arguments
-parser.add_argument('--polygon', help='Poligon of the wanted area to intersect with the search')
-parser.add_argument('--start-date', help='Start date of the search')
-parser.add_argument('--end-date', help='End date of the search')
-parser.add_argument('--path', help='Relative Orbit Path')
-parser.add_argument('--download', nargs='?', const='', default=None, help='Specify path to download the data, if not specified, the data will be downloaded either in SCRATCHDIR or HOME directory')
+parser.add_argument('--polygon', metavar='POLYGON', help='Poligon of the wanted area to intersect with the search')
+parser.add_argument('--start-date', metavar='DATE', help='Start date of the search')
+parser.add_argument('--end-date', metavar='DATE', help='End date of the search')
+parser.add_argument('--path', metavar='ORBIT', help='Relative Orbit Path')
+parser.add_argument('--download', metavar='FOLDER', nargs='?', const='', default=None, help='Specify path to download the data, if not specified, the data will be downloaded either in SCRATCHDIR or HOME directory')
 
 args = parser.parse_args()
-
 # (asf.constants.PRODUCT_TYPE)
-print(asf.constants.PRODUCT_TYPE)
-print(args.download)
 
 sdate = None
 edate = None
