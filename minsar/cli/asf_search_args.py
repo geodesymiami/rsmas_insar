9fcb9120a1ec1e79786f0ede4fafb7aa5fa149a4
disilvestro <ds96@live.it>
2024-02-27 11:45:42 -0500

    asf_search_args.py: added product option to choose between CLSLC, BURST, SLC

--- minsar/cli/asf_search_args.py
+++ minsar/cli/asf_search_args.py
@@ -10,15 +10,19 @@ import os
 # Define the description and epilog of the script
 EXAMPLE = """example:
   
+Date format: YYYYMMDD
+
 asf_search_args.py --polygon "POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))"
-asf_search_args.py --start-date 2019-01-01 --end-date 2021-09-29
+asf_search_args.py --start-date 20190101 --end-date 20210929 --path 59 --download $SCRATCHDIR/asf_product --product CSLC
 
 """
 
 workDir = 'SCRATCHDIR'
 
 # Create an ArgumentParser object
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description='Download or list Sentinel-1 data from the Alaska Satellite Facility',
+        formatter_class=argparse.RawTextHelpFormatter,
+        epilog=EXAMPLE)
 
 # Define your optional arguments
 parser.add_argument('--polygon', metavar='POLYGON', help='Poligon of the wanted area to intersect with the search')
@@ -26,6 +30,7 @@ parser.add_argument('--start-date', metavar='DATE', help='Start date of the sear
 parser.add_argument('--end-date', metavar='DATE', help='End date of the search')
 parser.add_argument('--path', metavar='ORBIT', help='Relative Orbit Path')
 parser.add_argument('--download', metavar='FOLDER', nargs='?', const='', default=None, help='Specify path to download the data, if not specified, the data will be downloaded either in SCRATCHDIR or HOME directory')
+parser.add_argument('--product', metavar='FILE', help='Choose the product type to download')
 
 inps = parser.parse_args()
 # (asf.constants.PRODUCT_TYPE)
@@ -35,12 +40,22 @@ edate = None
 polygon = None
 orbit = None
 path = None
+product = []
+
+if 'SLC' in inps.product:
+    product.append(asf.PRODUCT_TYPE.SLC)
+
+if 'BURST' in inps.product:
+    product.append(asf.PRODUCT_TYPE.BURST)
+
+if 'CSLC' in inps.product or inps.product is None: 
+    product.append(asf.PRODUCT_TYPE.CSLC)
 
 if inps.start_date is not None:
-    sdate = datetime.datetime.strptime(inps.start_date, '%Y-%m-%d').date()
+    sdate = datetime.datetime.strptime(inps.start_date, '%Y%m%d').date()
 
 if inps.end_date is not None:
-    edate = datetime.datetime.strptime(inps.end_date, '%Y-%m-%d').date()
+    edate = datetime.datetime.strptime(inps.end_date, '%Y%m%d').date()
 
 if inps.polygon is not None:
     polygon = inps.polygon
@@ -53,7 +68,7 @@ if inps.download is not None:
 
 results = asf.search(
     platform= asf.PLATFORM.SENTINEL1,
-    processingLevel=[asf.PRODUCT_TYPE.CSLC],
+    processingLevel=product,
     start = sdate,
     end = edate,
     intersectsWith = polygon,
