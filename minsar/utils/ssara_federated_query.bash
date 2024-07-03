a1eeae0525df3aa62dc241756c1f0189b02a633f
falkamelung <famelung@earth.miami.edu>
2024-07-02 21:15:06 -0500

    ssara_federated_query.bash: simplified and fixed missing quote issue

--- minsar/utils/ssara_federated_query.bash
+++ minsar/utils/ssara_federated_query.bash
@@ -1,77 +1,62 @@
 #!/usr/bin/env bash
 
 if [[ "$1" == "--help" || "$1" == "-h" ]]; then
-helptext="                                                                       \n\
+helptext="                                                                                                           \n\
    Downloads data.  It first run ssara_federated_query.py to create ssara_listing.txt and ssara_search*.kml files.   \n\
    Then it reads file URLs from ssara_listing.txt  and downloads using wget                                          \n\
-   (the options --download --print of sara_federated_query.py currently don't work).                                 \n
-  Examples:                                                                      \n\
-      ssara_federated_quey.bash --relativeOrbit=15 --intersectsWith='Polygon((4 49, 4 51, 10 51, 10 49, 4 49))' --platform=SENTINEL-1A,SENTINEL-1B --parallel=5 --maxResults=20000 \n\
-                                                                                 \n\
-   "
+                                                                                                                     \n\
+   Example:                                                                                                         \n\
+      ssara_federated_quey.bash --relativeOrbit=15 --intersectsWith='Polygon((4 49, 4 51, 10 51, 10 49, 4 49))' --platform=SENTINEL-1A,SENTINEL-1B --parallel=6 --maxResults=20000 \n\
+                                                                                                                     \n\
+"
    printf "$helptext"
    exit 0;
 fi
 
-# insert ' into intersectsWith string that is chopped off by bash
-copy=( "$@" )
-for i in "${!copy[@]}"; do
-    if [[ ${copy[$i]} == *intersectsWith=* ]]; then
-        tmp1=${copy[$i]:0:17}
-        tmp2=${copy[$i]:17:${#copy[$i]}}
-        #copy[$i]="$tmp1'$tmp2'"       
-        copy[$i]="$tmp1$tmp2"     # FA 6/2025: previous command this created for ManamSenDT60 an extra ''   --intersectsWith=''Polygon((142.00'
-        #copy[$i]="$tmp1\"$tmp2\""
-    elif [[ ${copy[$i]} == *collectionName=* ]]; then
-        tmp1=${copy[$i]:0:17}
-        tmp2=${copy[$i]:17:${#copy[$i]}}
-        copy[$i]="$tmp1'$tmp2'"
-    elif [[ ${copy[$i]} == --parallel* ]]; then
-        parallel=$(echo ${copy[$i]} | cut -d= -f2)
-    fi
-    #echo element $i: ${copy[$i]}
+cmd=""
+for arg in "$@"; do
+  if [[ "$arg" == --intersectsWith=* ]]; then
+    # Extract the value right after '=' and wrap it with single quotes
+    value="${arg#--intersectsWith=}"
+    arg="--intersectsWith='$value'"
+  fi
+  if [[ "$arg" == --parallel* ]]; then
+    parallel="${arg#--parallel=}"
+  fi
+  # Append each argument to the command
+  cmd+=" $arg"
 done
 
-echo "$(date +"%Y%m%d:%H-%m") * `basename "$0"` "${copy[@]}" " >> log
-
-if  [[ -z "$parallel" ]] ; then 
-   parallel=5 
-fi
-
-timeout=500
-
-#echo "parallel=${parallel}"
-
-# select password according to satellite
-if [[ ${copy[@]} == *SENTINEL* ]]; then
-   user=`grep asfuser $RSMASINSAR_HOME/tools/SSARA/password_config.py | sed 's/\"//g''' | cut -d '=' -f 2`
-   passwd=`grep asfpass $RSMASINSAR_HOME/tools/SSARA/password_config.py | sed 's/\"//g''' | cut -d '=' -f 2`
-elif [[ ${copy[@]} == *COSMO-SKYMED* ]] || [[ ${copy[@]} == *ALOS-2* ]] || [[ ${copy[@]} == *TSX* ]]; then
-   user=`grep unavuser $RSMASINSAR_HOME/tools/SSARA/password_config.py | sed 's/\"//g''' | cut -d '=' -f 2`
-   passwd=`grep unavpass $RSMASINSAR_HOME/tools/SSARA/password_config.py | sed 's/\"//g''' | cut -d '=' -f 2`
-   regex="https:\/\/imaging\.unavco\.\.org\/[a-zA-Z\/0-9\_]+\.tar\.gz"
-   regex="https:\/\/imaging\.unavco\.\.org\/*\.gz"
-fi
-
-rm -f ssara_listing.txt
-argv=( "$@" )
-string_for_display=$(printf " %s" "${copy[@]}")
-string_for_display=${string_for_display:1}
-#echo "QQ string_fo_display: $string_for_display"
+# writing command to logfile including '' for intersectsWith
+echo "$(date +"%Y%m%d-%H:%m") * `basename "$0"` $cmd " >> log
 
 asfResponseTimeout_opt="--asfResponseTimeout=300"
 if [[ $string_for_display == *TSX* ]] || [[ $string_for_display == *CSK* ]]; then
    asfResponseTimeout_opt=""
 fi
 
-#cmd="ssara_federated_query.py $string_for_display $asfResponseTimeout_opt --kml --print  > ssara_listing.txt"
-cmd="ssara_federated_query.py $string_for_display $asfResponseTimeout_opt --kml --print  > ssara_listing.txt 2>ssara.e"
-echo "Running ... $cmd"
+# run ssara_federated_query.py to create ssara_listing.txt
+rm -f ssara_listing.txt
+cmd="ssara_federated_query.py $cmd $asfResponseTimeout_opt --kml --print > ssara_listing.txt 2> ssara.e"
+echo "$(date +"%Y%m%d-%H:%m") * $cmd " >> log
+echo "Running.... $cmd"
 eval "$cmd"
+
+# select password according to satellite
+if [[ $cmd == *SENTINEL* ]]; then
+   user=`grep asfuser $RSMASINSAR_HOME/tools/SSARA/password_config.py | sed 's/\"//g''' | cut -d '=' -f 2`
+   passwd=`grep asfpass $RSMASINSAR_HOME/tools/SSARA/password_config.py | sed 's/\"//g''' | cut -d '=' -f 2`
+elif [[ $cmd == *COSMO-SKYMED* ]] || [[ $cmd == *ALOS-2* ]] || [[ $cmd == *TSX* ]]; then
+   user=`grep unavuser $RSMASINSAR_HOME/tools/SSARA/password_config.py | sed 's/\"//g''' | cut -d '=' -f 2`
+   passwd=`grep unavpass $RSMASINSAR_HOME/tools/SSARA/password_config.py | sed 's/\"//g''' | cut -d '=' -f 2`
+   regex="https:\/\/imaging\.unavco\.\.org\/[a-zA-Z\/0-9\_]+\.tar\.gz"
+   regex="https:\/\/imaging\.unavco\.\.org\/*\.gz"
+fi
+
 grep -q "urllib.error.HTTPError: HTTP Error 502: Proxy Error" ssara.e && { echo "Download problem: urllib.error.HTTPError: HTTP Error 502: Proxy Error"; exit 1; }
 
 downloads_num=$(grep Found ssara_listing.txt | cut -d " " -f 2)
-echo "Number of granules: $downloads_num"
+echo "Number of granules in ssara_listing.txt : $downloads_num"
 
 urls_list=$(cut -s -d ',' -f 14 ssara_listing.txt)
 unset IFS
@@ -82,11 +67,17 @@ num_urls=${#urls[@]}
 echo "URLs to download: ${urls[@]}"
 echo "$(date +"%Y%m%d:%H-%m") * Datafiles to download: $num_urls" | tee -a log
 
-echo "downloading using:: xargs -n 1 -P $parallel timeout $timeout wget --continue --user $user --password $passwd"
+if  [[ -z "$parallel" ]] ; then
+   parallel=5
+fi
+
+timeout=500
+
+echo "downloading using: echo URLs | xargs -n 1 -P $parallel timeout $timeout wget --continue --user $user --password $passwd" | tee -a log
 echo ${urls[@]} | xargs -n 1 -P $parallel timeout $timeout wget --continue --user $user --password $passwd
 exit_code=$?
 
-echo "$(date +"%Y%m%d:%H-%m") check_download: `check_download.py $PWD --delete`"  | tee -a log
+echo "$(date +"%Y%m%d-%H:%m") check_download: `check_download.py $PWD --delete`"  | tee -a log
 granules_num=$(ls *.{zip,tar.gz} 2> /dev/null | wc -l)
 echo "$(date +"%Y%m%d:%H-%m") * Downloaded scenes after check_download: $granules_num" | tee -a log
 
@@ -113,7 +104,6 @@ echo "$(date +"%Y%m%d:%H-%m") check_download: `check_download.py $PWD --delete`"
 granules_num=$(ls *.{zip,tar.gz} 2> /dev/null | wc -l)
 echo "$(date +"%Y%m%d:%H-%m") * Downloaded scenes after check_download: $granules_num" | tee -a log
 
-
 if [[ $granules_num -ge $num_urls ]]; then
    echo "Download was successful, downloaded scenes: $granules_num" | tee -a log
    exit 0;
@@ -121,26 +111,4 @@ else
   echo "Not all scenes downloaded, downloaded scenes: $granules_num" | tee -a log
   exit 1;
 fi
-
-# start=0
-# stop=$(($start+$parallel))
-# while [ $start -le $num_urls ]; do
-#     us="${urls[@]:$start:$parallel}"
-#     echo "URLs to download: ${us[@]}"
-#     echo $us | xargs -n 1 -P $parallel timeout $timeout wget --continue --user $user --password $passwd -nv
-#     exit_code=$?
-#     runs=1
-#     while [ $exit_code -eq 123 -o $exit_code -eq 127 ] && [ $runs -lt 3 ]; do
-#         echo "Something went wrong. Exit code was ${exit_code}. Trying again with ${t} second timeout."
-#         echo "$(date +"%Y%m%d:%H-%m") * Something went wrong. Exit code was ${exit_code}. Trying again with ${t} second timeout" >> log
-#         echo $us | xargs -n 1 -P $parallel timeout $timeout wget --continue --user $user --password $passwd -nv
-#         exit_code=$?
-#         runs=$((runs+1))
-#         sleep 60
-#     done
-#     echo "Finished downloading files $stop/$num_urls succesfully."
-#     start=$(($stop+1))
-#     stop=$(($start+$parallel))
-# done
-
 
