2ee36d9a411d287a253b29cda2bec5771c4096b1
falkamelung <famelung@earth.miami.edu>
2024-08-15 03:36:20 -0500

    adjusted to work from any directory

--- setup/install_credential_files.bash
+++ setup/install_credential_files.bash
@@ -1,31 +1,33 @@
 #!/usr/bin/env bash
 ######### copy credentials to right place ##############
 
-# for ssara 
-characterCount=`wc -m ../tools/SSARA/password_config.py`
-characterCount=$(echo "${characterCount[0]%% *}")
+# Determine the script's directory
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
-if [[  $characterCount == 75 ]]; then
-      echo "Use default password_config.py for SSARA (because existing file lacks passwords)"
-      echo "Copying password_config.py into ../tools/SSARA"
-      cp ~/accounts/password_config.py ../tools/SSARA
-   else
-      echo File password_config.py not empty - kept unchanged
+# for ssara
+SSARA_FILE="$SCRIPT_DIR/../tools/SSARA/password_config.py"
+characterCount=$(wc -m < "$SSARA_FILE")
+
+if [[ $characterCount == 75 ]]; then
+  echo "Use default password_config.py for SSARA (because existing file lacks passwords)"
+  echo "Copying password_config.py into $SCRIPT_DIR/../tools/SSARA"
+  cp ~/accounts/password_config.py "$SCRIPT_DIR/../tools/SSARA"
+else
+  echo "File password_config.py not empty - kept unchanged"
 fi
 
-# for dem.py 
+# for dem.py
 if [[ ! -f ~/.netrc ]]; then
   echo "copying .netrc file for DEM data download into ~/.netrc"
   cp ~/accounts/netrc ~/.netrc
 fi
 
-# for pyaps 
-python_version=$(echo "python3.$(../tools/miniconda3/bin/python --version | cut -d. -f2)")
-model_cfg_file=$(echo "../tools/miniconda3/lib/$python_version/site-packages/pyaps3/model.cfg")
-if [[ ! -f $model_cfg_file ]]; then
-      echo "Copying default model.cfg for ECMWF download with PyAPS into $(dirname $model_cfg_file)"
-      cp ~/accounts/model.cfg $model_cfg_file
-   else
-      echo File model.cfg exists already - kept unchanged
+# for pyaps
+PYTHON_VERSION=$(echo "python3.$("$SCRIPT_DIR/../tools/miniconda3/bin/python" --version | cut -d. -f2)")
+MODEL_CFG_FILE="$SCRIPT_DIR/../tools/miniconda3/lib/$PYTHON_VERSION/site-packages/pyaps3/model.cfg"
+if [[ ! -f $MODEL_CFG_FILE ]]; then
+  echo "Copying default model.cfg for ECMWF download with PyAPS into $(dirname "$MODEL_CFG_FILE")"
+  cp ~/accounts/model.cfg "$MODEL_CFG_FILE"
+else
+  echo "File model.cfg exists already - kept unchanged"
 fi
-
