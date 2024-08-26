20c815374e640e1b5c5f89deed338135bc14b49c
falkamelung <famelung@earth.miami.edu>
2024-08-25 19:04:46 -0500

    update_minTempCoh.bash: minor update

--- minsar/utils/update_minTempCoh.bash
+++ minsar/utils/update_minTempCoh.bash
@@ -12,6 +12,29 @@ show_help() {
     echo "  update_minTempCoh.bash \$TE/unittestGalapagosSenDT128.template miaplpy/network_single_reference"
     echo "  update_minTempCoh.bash \$TE/unittestGalapagosSenDT128.template 0.9 miaplpy/network_single_reference"
 }
+###################################################################################
+function create_template_array() {
+mapfile -t array < <(grep -e ^minsar -e ^mintpy -e ^miaplpy $1)
+declare -gA template
+for item in "${array[@]}"; do
+  #echo "item: <$item>"
+  IFS='=' ; read -a arr1 <<< "$item"
+  item="${arr1[1]}"
+  IFS='#' ; read -a arr2 <<< "$item"
+  key="${arr1[0]}"
+  key=$(echo $key | tr -d ' ')
+  value="${arr2[0]}"
+  shopt -s extglob
+  value="${value##*( )}"          # Trim leading whitespaces
+  value="${value%%*( )}"          # Trim trailing whitespaces
+  shopt -u extglob
+  #echo "key, value: <$key> <$value>"
+  if [ ! -z "$key"  ]; then
+     template[$key]="$value"
+  fi
+unset IFS
+done
+}
 
 get_project_dir() {
     local template_path="$1"
@@ -42,11 +65,14 @@ run_update() {
     local data_dir="$3"
     local project_dir
 
+    create_template_array $template_path
+
     project_dir=$(get_project_dir "$template_path")
     cd "${SCRATCHDIR}/${project_dir}" || { echo "Failed to change directory to $project_dir"; exit 1; }
+    touch "${data_dir}/temporalCoherence.h5"
     touch "${data_dir}/geo/geo_temporalCoherence.h5"
     if [[ -n "$min_temp_coh" ]]; then
-        echo "Minimum temporal coherence: ${min_temp_coh}"
+        echo "Updating $project_name using minimum temporal coherence (mintpy.timeseries.minTempCoh) of  ${min_temp_coh}"
         replace_min_temp_coh "$template_path" "$min_temp_coh"
     fi
 
@@ -59,6 +85,11 @@ run_update() {
         joblast_id=$(echo "$joblast_output" | awk 'END {print $NF}')
         echo "Second job submitted with ID: $joblast_id"
     else
+
+        # FA 8/24  This need to be in jobfile
+        cmd="generate_mask.py ${data_dir}/temporalCoherence.h5 -m ${template[mintpy.networkInversion.minTempCoh]}  --nonzero -o ${data_dir}/maskTempCoh.h5"
+        eval "$cmd"
+
         sed -i "s|SBATCH -t .:..:..|SBATCH -t 1:59:00|g" ${data_dir}/run_files/run_09_mintpy_timeseries_correction_0.job
         job1_output=$(sbatch -p $QUEUE_DEV ${data_dir}/run_files/run_09_mintpy_timeseries_correction_0.job)
         job1_id=$(echo "$job1_output" | awk 'END {print $NF}')
