9634283e09ea957bfd34b95c78626954e8806dd0
falkamelung <famelung@earth.miami.edu>
2025-05-08 15:26:03 -0500

    check_proj.bash: simplified

--- minsar/utils/check_proj.bash
+++ minsar/utils/check_proj.bash
@@ -26,16 +26,16 @@ EOF
 fi
 
 # --- Test transform ---
-echo "Checking if gdaltransform supports vertical transformations..."
+echo "Checking if gdaltransform supports vertical transformations. Running..."
+echo "echo \"-80.125 25.9 0 \" | gdaltransform -s_srs EPSG:4326+5773 -t_srs EPSG:4326+4979"
 
 RESULT=$(echo "-80.125 25.9 0" | gdaltransform -s_srs EPSG:4326+5773 -t_srs EPSG:4326+4979 2>&1)
+HEIGHT=$(echo "$RESULT" | awk '{print $3}')
+ROUNDED=$(printf "%.1f\n" "$HEIGHT")
+#echo "Ellipsoidal height at (-80.125, 25.9, 0) is approximately: ${ROUNDED} m"
 
-if echo "$RESULT" | grep -Eq '^[+-]?[0-9]+\.[0-9]+ +[+-]?[0-9]+\.[0-9]+ +[+-]?[0-9]+\.[0-9]+'; then
-    HEIGHT=$(echo "$RESULT" | awk '{print $3}')
-    ROUNDED=$(printf "%.1f\n" "$HEIGHT")
+if [[ $ROUNDED == -28.0 ]]; then
     echo "Success: Vertical transformation is supported."
 else
     echo "ERROR: geoid-to-ellipsoid transformation fails inside GDAL. PROJ not properly install or missing grid file (e.g., egm96_15.gtx)."
-    echo "Raw output:"
-    echo "$RESULT"
 fi
