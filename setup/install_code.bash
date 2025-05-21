6de7d284ed36fdf49033f0a61b2f1bbc0543a09d
Falk Amelung <falkamelung@users.noreply.github.com>
2025-05-21 07:26:45 -0400

    Update install_code.bash
    ISCE operations only for Linux

--- setup/install_code.bash
+++ setup/install_code.bash
@@ -93,17 +93,20 @@ cp -p minsar/additions/mintpy/cli/save_hdfeos5.py tools/MintPy/src/mintpy/cli/
 cp -p minsar/additions/miaplpy/prep_slc_isce.py tools/MiaplPy/src/miaplpy
 
 ### Adding ISCE fixes and copying checked-out ISCE version (the latest) into miniforge directory ###
+if [[ "$(uname)" == "Linux" ]]; then
 cp -p minsar/additions/isce/logging.conf tools/miniforge3/lib/python3.?/site-packages/isce/defaults/logging/logging.conf
 cp -p minsar/additions/isce2/topsStack/FilterAndCoherence.py tools/isce2/contrib/stack/topsStack
 cp -p minsar/additions/isce2/stripmapStack/prepRawCSK.py tools/isce2/contrib/stack/stripmapStack
 cp -p minsar/additions/isce2/stripmapStack/unpackFrame_TSX.py tools/isce2/contrib/stack/stripmapStack
 cp -p minsar/additions/isce2/DemStitcher.py tools/isce2/contrib/demUtils/demstitcher
 
+
 ### Copying ISCE fixes into miniforge directory ###
 cp -r tools/isce2/contrib/stack/* tools/miniforge3/share/isce2
 cp -r tools/isce2/components/isceobj/Sensor/TOPS tools/miniforge3/share/isce2
 cp tools/isce2/components/isceobj/Sensor/TOPS/TOPSSwathSLCProduct.py tools/miniforge3/lib/python3.?/site-packages/isce/components/isceobj/Sensor/TOPS
 cp tools/isce2/contrib/demUtils/demstitcher/DemStitcher.py  tools/miniforge3/lib/python3.??/site-packages/isce/components/contrib/demUtils
+fi
 
 ### Create orbits and aux directories
 echo "mkdir -p $SENTINEL_ORBITS $SENTINEL_AUX"
